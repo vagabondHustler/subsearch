d24e668e7994d100a927b127f2d1cf0acbf3b4ad
vagabondHustler <vagabondhustler.github@gmail.com>
2021-11-11 21:03:59 +0100

    class Search added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import webbrowser                # open website URL in deafult browser
-import os
+import webbrowser       # open website URL in deafult browser
+import os               # get cwd
 
 
 '''
@@ -14,24 +14,24 @@ def current_working_dir():
     return dir_name                                     # return the value, used in title_filter
 
 
-def title_filter(dir_name: str, release_title_lst=[]):  # cwd, e.g: C:/Users/username/Downloads/foo.2021.1080p.WEB.H264-bar
-    dir_name_lst = dir_name.split('\\')                 # removes / form the path to the directry e.g: 'C:' 'Users' 'username' 'Downloads' 'foo.2021.1080p.WEB.H264-bar'
-    release_dot_name = dir_name_lst[-1]                 # get last part of the path which is the release name with . as spaces e.g: foo.2021.1080p.WEB.H264-bar
-    release_name_lst = release_dot_name.split('.')      # remove . from the release name e.g: 'foo' '2021' '1080p' 'WEB' 'H264-bar'
-    for word in release_name_lst:                       # loop through lst
-        try:                                            # if word is not a int ValueError is raised
-            int(word)
-            break                                       # if word is a in break
-        except ValueError:
-            release_title_lst.append(word)              # appends the Title to from the release name
-    name_of_release = ' '.join(release_title_lst)       # join the lst into str with spaces between words
-    return name_of_release
+class Search:
+    def filter(dir_name: str, release_title_lst=[]):        # cwd, e.g: C:/Users/username/Downloads/foo.2021.1080p.WEB.H264-bar
+        dir_name_lst = dir_name.split('\\')                 # removes / form the path to the directry e.g: 'C:' 'Users' 'username' 'Downloads' 'foo.2021.1080p.WEB.H264-bar'
+        release_dot_name = dir_name_lst[-1]                 # get last part of the path which is the release name with . as spaces e.g: foo.2021.1080p.WEB.H264-bar
+        release_name_lst = release_dot_name.split('.')      # remove . from the release name e.g: 'foo' '2021' '1080p' 'WEB' 'H264-bar'
+        for word in release_name_lst:                       # loop through lst
+            try:                                            # if word is not a int ValueError is raised
+                int(word)
+                break                                       # if word is a in break
+            except ValueError:
+                release_title_lst.append(word)              # appends the Title to from the release name
+        name_of_release = ' '.join(release_title_lst)       # join the lst into str with spaces between words
+        return name_of_release
+
+    def subscene(name_of_release: str):
+        query = 'https://subscene.com/subtitles/searchbytitle?query='
+        url = f'{query}{name_of_release}'
+        webbrowser.open(url)                                # returns name of release with year eg 'foo 2021'
 
 
-def search_subscene(name_of_release: str):
-    query = 'https://subscene.com/subtitles/searchbytitle?query='
-    url = f'{query}{name_of_release}'
-    webbrowser.open(url)                                # returns name of release with year eg 'foo 2021'
-
-
-search_subscene(title_filter(current_working_dir()))
+Search.subscene(Search.filter(current_working_dir()))
