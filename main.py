793d1ef8b424c3c61bb5968358b20d1d2cf786af
vagabondHustler <vagabondhustler.github@gmail.com>
2021-11-11 05:15:35 +0100

    Added comment, basic of what code does

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from bs4 import BeautifulSoup    # pulling data out of HTML/XML files
 from tkinter import filedialog   # folder dialog
 
 
+'''
+    The standard for naming a scene release is typically by Title.Year.Soruce.Codec-GroupName, e.g foo.2021.1080p.WEB.H264-bar
+    This standard can be used to filter everything but Title and Year and use it as str to search with
+    The str can be aquired from selecting the scene release directory
+'''
+
+
