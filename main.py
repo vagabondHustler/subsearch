5d4cf4c84422fa6370f3d6a1654ee1346afb05fa
vagabondHustler <vagabondhustler.github@gmail.com>
2021-11-11 05:14:59 +0100

    Import models

---
+++ main.py
@@ -0,0 +1,6 @@
+import requests                  # HTTP libary to easily send requests
+import webbrowser                # open website URL in deafult browser
+from bs4 import BeautifulSoup    # pulling data out of HTML/XML files
+from tkinter import filedialog   # folder dialog
+
+
