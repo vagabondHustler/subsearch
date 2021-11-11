478a8cf8994fecbe17dcc19b43f52ac4c16c6d49
vagabondHustler <vagabondhustler.github@gmail.com>
2021-11-11 05:16:22 +0100

    Func, filter path, soruce, codec, groupname from search parameter

--- main.py
+++ main.py
@@ -19,3 +19,19 @@ def select_release_dir():
         return dir_name     # return the value, used in title_filter
 
 
+def title_filter(release_title_lst=[]):
+    dir_name = select_release_dir()                     # directory prompt, e.g: C:/Users/username/Downloads/foo.2021.1080p.WEB.H264-bar
+    dir_name_lst = dir_name.split('/')                  # removes / form the path to the directry e.g: 'C:' 'Users' 'username' 'Downloads' 'foo.2021.1080p.WEB.H264-bar'
+    release_dot_name = dir_name_lst[-1]                 # get last part of the path which is the release name with . as spaces e.g: foo.2021.1080p.WEB.H264-bar
+    release_name_lst = release_dot_name.split('.')      # remove the . from the release name e.g: 'foo' '2021' '1080p' 'WEB' 'H264-bar'
+    for word in release_name_lst:                       # loop through lst
+        try:                                            # if word is not a int ValueError is raised
+            int(word)
+            release_title_lst.append(word)              # if word is a Year it gets append to lst
+            break                                       # breaks the for loop and leaves the rest of the release name
+        except ValueError:
+            release_title_lst.append(word)              # appends the Title to from the release name
+    name_of_release = ' '.join(release_title_lst)       # join the lst into str with spaces between words
+    return name_of_release                              # returns name of release with year eg 'foo 2021'
+
+
