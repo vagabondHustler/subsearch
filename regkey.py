35682baf40660eb5053ecc14123f0d6599c09fc6
vagabondHustler <vagabondhustler.github@gmail.com>
2021-11-12 01:22:15 +0100

    Bug fixed

--- regkey.py
+++ regkey.py
@@ -1,18 +1,16 @@
 from os import getcwd
 
-cwd = getcwd()
 
-
-def write():
+def write_key():
     regkey_file = open('regkey.reg', 'w')
     regkey = r'''Windows Registry Editor Version 5.00
 
 [HKEY_CLASSES_ROOT\Directory\Background\shell\Search subscene]
 @=""
-"Icon"="'''+str(cwd()).replace('\\', '\\\\')+r'''\\resources\\icon.ico, 0"
+"Icon"="'''+str(getcwd()).replace('\\', '\\\\')+r'''\\resources\\icon.ico, 0"
 
 [HKEY_CLASSES_ROOT\Directory\Background\shell\Search subscene\command]
-@="cmd.exe \"%V\" /c  python '''+str(cwd()).replace('\\', '\\\\')+r'\\main.py"'
+@="cmd.exe \"%V\" /c  python '''+str(getcwd()).replace('\\', '\\\\')+r'\\main.py"'
 
     regkey_file.write(str(regkey))
     regkey_file.close()
