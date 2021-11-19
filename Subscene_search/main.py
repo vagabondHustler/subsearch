e580c8e34be34c52bfea0897fb8edeeed61889e6
vagabondHustler <vagabondhustler.github@gmail.com>
2021-11-19 11:19:55 +0100

    Reformatted some code

--- Subscene_search/main.py
+++ Subscene_search/main.py
@@ -23,6 +23,7 @@ def cwd():
 def unzipper():
     dir_name = cwd()
     ext = ".zip"
+
     for item in os.listdir(dir_name):               # loop through items in dir
         if item.endswith(ext):                      # check for ".zip" extension
             file_name = os.path.abspath(item)       # get full path of files
@@ -38,12 +39,14 @@ class Registry:
         try:
             with reg.ConnectRegistry(None, reg.HKEY_CLASSES_ROOT) as hkey:
                 reg.OpenKey(hkey, sub_key)
+
         except Exception:                                                   # raised if no key found
             return False
 
     def is_admin(self):
         try:
             return ctypes.windll.shell32.IsUserAnAdmin()        # check if script ran as admin, otherwise import .reg is denied
+
         except PermissionError:                                 # raiser if user did not run as admin
             return False
 
@@ -54,6 +57,7 @@ class Search:
         dir_name_lst = dir_name.split('\\')                      # removes / form the path to the directry e.g: 'C:' 'Users' 'username' 'Downloads' 'foo.2021.1080p.WEB.H264-bar'
         release_dot_name = dir_name_lst[-1]                      # get last part of the path which is the release name with . as spaces e.g: foo.2021.1080p.WEB.H264-bar
         release_name_lst = release_dot_name.split('.')           # remove . from the release name e.g: 'foo' '2021' '1080p' 'WEB' 'H264-bar'
+
         for word in release_name_lst:                            # loop through lst
             try:                                                 # if word is not a int ValueError is raised
                 int(word)
@@ -64,6 +68,7 @@ class Search:
                     title = word
                 else:
                     title = ' '.join(words_lst)
+
         if use == 'scene_group':                            # returns the scene group e.g bar
             scene_group = dir_name_lst[-1].split('-')
             return scene_group[-1]
@@ -87,6 +92,7 @@ class Webscraping:
                  url=s.parameter(use='url'),                    # returns release title e.g foo
                  scene_group=s.parameter(use='scene_group'),    # returns initial search url
                  search_title_lst=[], links_to_dl=[]):          # lst
+
         self.title = title
         print(f'Title found: {title}')
         self.scene_name = scene_name
@@ -106,6 +112,7 @@ class Webscraping:
         doc = BeautifulSoup(source, 'html.parser')                                      # computing html
         search_result = doc.find('div', class_='search-result')                         # section with search result from initial search
         links = [a['href'] for a in search_result.find_all('a', href=True) if a.text]   # url of subtitle matching title name
+
         for link in links:                                                              # place urls in said lst
             self.search_title_lst.append(f'https://subscene.com/{link}')                # add missing address to url
         number = len(self.search_title_lst)
@@ -122,6 +129,7 @@ class Webscraping:
         else:
             print('tbody is none')
             return
+
         for tr in trs:
             if 'English' in tr.text:                                                    # filter out all non-english subtitles
                 release_name = [(x.text.replace('\r\n\t\t\t\t\t\t', '').replace(' \r\n\t\t\t\t\t', '')) for x in tr.find_all('span')]   # remove spaces, tabs new-lines etc
@@ -132,11 +140,13 @@ class Webscraping:
                         self.links_to_dl.append(f'https://subscene.com/{link[0]}')
                     else:
                         pass
+
         return self.links_to_dl
 
     def download_sub(self):                         # download .zip files containing the subtitles
         print('\nGathering download links')
         number = 0
+
         for url in self.links_to_dl:                # lst containing urls with subtitles to download
             number += 1
             save_path = cwd()
@@ -144,10 +154,15 @@ class Webscraping:
             source = requests.get(url).text
             doc = BeautifulSoup(source, 'html.parser')
             link = [dl['href'] for dl in doc.find_all('a', href=True, id='downloadButton')]     # the download link of the .zip-file
-            author = [(a.text.replace('A commentary by', '').replace('\n', '').replace('\r', '').replace('\t', '').replace(' ', '')) for a in doc.find_all('li', class_='author') if a.text]   # remove spaces, tabs new-lines etc
+            # remove spaces, tabs new-lines etc
+            author = [
+                (a.text.replace('A commentary by', '').replace('\n', '').replace('\r', '').replace('\t', '').replace(' ', ''))
+                for a in doc.find_all('li', class_='author') if a.text
+            ]
             print(f'\nDownloading subtitles for {name} as {name}_by_{author[0]}_{number}.zip')
             zip_file = f'{save_path}\\{name}_by_{author[0]}_{number}.zip'       # name and path of .zip
             zip_file_url = f'https://subscene.com/{link[0]}'                    # add missing address to url
+
             r = requests.get(zip_file_url, stream=True)
             with open(zip_file, 'wb') as fd:                                    # save .zip with for loop
                 for chunk in r.iter_content(chunk_size=128):
@@ -174,19 +189,19 @@ class Webscraping:
         print(f'Best matching .srt: {new_name}\nRest of .srt-files moved to ~/subs\n')
 
 
-r = Registry()
-
-
 def main():     # main, checks if user is admin, if registry for contextmenu exists, runs webscraping etc...
+    r = Registry()
     if r.is_admin():
         regkey.write_key()                              # regkey.reg gets written, adds a context menu option to start main.py when right clicking inside folder
         os.system('cmd /c "reg import regkey.reg"')     # imports regkey.reg to the registry
     if r.is_key() is False:                             # check if key exists
         # Re-run the program with admin rights
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)  # runs script as admin if not admin
+
     else:
         w = Webscraping()
         number = len(w.search_title())
+
         for x in range(number):
             print(f'\nChecking {x+1} out of {number}')
             w.check_titles(x)
@@ -201,5 +216,6 @@ def main():     # main, checks if user is admin, if registry for contextmenu exi
         unzipper()
         w.rename_srt()
         print('---*** All done ***---')
+
 
 main()
