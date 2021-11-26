c31b5add1f94fa5df0206f1ae0982ab9e9abfbd3
vagabondHustler <vagabondhustler.github@gmail.com>
2021-11-26 11:34:04 +0100

    Changed name of lst

--- Subscene_search/main.py
+++ Subscene_search/main.py
@@ -86,8 +86,8 @@ class Gui():    # main gui
         label.configure(bg=self.bg, fg=self.fg, font=self.font8)
         label.place(relx=0, rely=0.0078, anchor='nw')
 
-    def lang_menu(self, master):        # language
-        rd = Redirect()                 # custom print class
+    def lang_menu(self, master):            # language
+        rd = Redirect()                     # custom print class
 
         def select(self):                   # allows selection of language from list
             selection = lb.curselection()
@@ -178,7 +178,7 @@ class Gui():    # main gui
     offsety = 0
 
 
-lst = []
+_output_lst = []
 
 
 class Redirect:             # class for printing to Gui terminal
@@ -186,22 +186,22 @@ class Redirect:             # class for printing to Gui terminal
         pass
 
     def print(self, x):
-        lst.append(f'  {x}')
+        _output_lst.append(f'  {x}')
         lbc = Listbox(root, height=24, width=120, selectmode='single', exportselection=False, activestyle='none')
         lbc.configure(bd=0, highlightthickness=0,
                       bg='#121212', fg='#797979', highlightcolor='#121212', selectbackground='#240d14', selectforeground='#797979', font='Cascadia 8 bold')
         lbc.grid(column=0, padx=6, pady=30, row=1)
 
-        for item in lst:
-            lbc.insert(END, item)
-        lbc.select_clear(lbc.size() - 2)  # Clear the current selected item
-        lbc.select_set(END)  # Select the new item
+        for item in _output_lst:
+            lbc.insert(END, item)           # insert from _output_lst
+        lbc.select_clear(lbc.size() - 2)    # Clear the current selected item
+        lbc.select_set(END)                 # Select the new item
         lbc.yview(END)
-        if '--- All done ---' in lst[-1]:
+        if '--- All done ---' in _output_lst[-1]:
             for number in range(3, -1, -1):
                 lbc.insert(END, f'  Exit in {number} seconds')
-                lbc.select_clear(lbc.size() - 2)  # Clear the current selected item
-                lbc.select_set(END)  # Select the new item
+                lbc.select_clear(lbc.size() - 2)
+                lbc.select_set(END)
                 lbc.yview(END)
                 time.sleep(1)
 
