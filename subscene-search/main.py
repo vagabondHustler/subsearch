176f715ee2e3b57b3a8f1ae423813072c3f5d388
vagabondHustler <vagabondhustler.github@gmail.com>
2021-12-09 20:23:29 +0100

    Added comments

--- subscene-search/main.py
+++ subscene-search/main.py
@@ -1,4 +1,3 @@
-# main
 import time
 
 from src import user_config
@@ -13,6 +12,9 @@ from src.tools import file_manager as fm
 
 
 def main() -> None:
+    #
+    # initialising
+    #
     start = time.perf_counter()
     if got_key() is False:
         user_config.context_menu()
@@ -23,6 +25,9 @@ def main() -> None:
         user_config.select_language()
 
     scrape = SearchSubscene()
+    #
+    # fetch language
+    #
     language: str = get("language")
     languages: list = get("languages")
     for abbr_num, abbr in enumerate(languages):
@@ -31,12 +36,14 @@ def main() -> None:
             abbr_supported = True
             break
         elif abbr_num >= len(languages):
-            log_msg("[ERROR] Your language is not fully supported")
-            log_msg("[ERROR] See supported languages in subscene-search/config/languages.txt")
-            log_msg("[ERROR] Search might be longer for TV-series")
+            log_msg("[ERROR]: Your language is not fully supported")
+            log_msg("[ERROR]: See supported languages in subscene-search/config/languages.txt")
+            log_msg("[ERROR]: Search might be longer for TV-series")
             language_abbr = "en"
             abbr_supported = False
-
+    #
+    # filter parameters
+    #
     param = get_parameters(cwd().lower(), language_abbr)
     log_msg("[PARAMETERS]")
     log_msg(f"[LANGUAGE]: {language}, {language_abbr}")
@@ -48,7 +55,9 @@ def main() -> None:
     log_msg(f"[RELEASE]: {param.release}")
     log_msg(f"[GROUP]: {param.group}")
     log_msg(f"[URL]: {param.url}\n")
-
+    #
+    # get title
+    #
     to_be_scraped: list = []
     title_keys = scrape.search_for_title(param.url)
     for key, value in zip(title_keys, title_keys.values()):
@@ -73,7 +82,9 @@ def main() -> None:
         elapsed = time.perf_counter() - start
         log_msg(f"[subsecen-search]: Finished in {elapsed} seconds.\n\n")
         return
-
+    #
+    # search title for subtitle
+    #
     to_be_downloaded: list = []
     while len(to_be_scraped) > 0:
         for url in to_be_scraped:
@@ -89,7 +100,9 @@ def main() -> None:
                 to_be_downloaded.append(value) if value not in to_be_downloaded else None
         to_be_scraped.pop(0) if len(to_be_scraped) > 0 else None
         log_msg("[Done]\n") if len(to_be_downloaded) > 0 else None
-
+    #
+    # check if subtitle is a match, if so download
+    #
     if len(to_be_downloaded) == 0:
         log_msg(f"[subsecen-search]: No subtitles to download for {param.release}")
         elapsed = time.perf_counter() - start
@@ -102,7 +115,9 @@ def main() -> None:
         file_path = f"{cwd()}\\{enu_num}.zip"
         log_msg(f"[Downloading]: {enu_num}/{len(to_be_downloaded)}.zip")
         fm.download_zip(file_path, root_dl_url)
-
+    #
+    # extract files, rename files, delete unsued files
+    #
     log_msg(f"[Extracting]: .srt's from .zip's")
     fm.extract_zips(cwd(), ".zip")
     log_msg(f"[Re-naming]: Best matching .srt to {param.release}.srt")
