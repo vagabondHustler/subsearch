c2e9040561216af08fc1537ac901c00b7fe24f00
vagabondHustler <vagabondhustler.github@gmail.com>
2021-12-10 16:48:36 +0100

    Removed unnecessary else

--- subscene-search/src/tools/compare.py
+++ subscene-search/src/tools/compare.py
@@ -18,14 +18,10 @@ def check(searched: str, search_result: str) -> Precentage:
     elif len(searched) > len(search_result):
         for ga in range(greater_answer):
             search_result.append("_None")
-    else:
-        pass
 
     for x, y in zip(searched, search_result):
-        if compare(x, y) is True:
+        if compare(x, y):
             match += 1
-        else:
-            pass
 
     tot = len(searched)
     precentage = round((match / tot) * 100)
@@ -54,5 +50,4 @@ def is_bigger(searched, search_result) -> int:
 def compare(x, y) -> bool:
     if x.lower() == y.lower():
         return True
-    else:
-        return False
+    return False
