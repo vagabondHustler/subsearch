2785052de1f48a5d723cc4a55d0781d7c6883312
vagabondHustler <vagabondhustler.github@gmail.com>
2021-12-11 17:42:07 +0100

    Added menu_option 4 now works

--- subscene-search/configure.py
+++ subscene-search/configure.py
@@ -6,6 +6,7 @@ from src.current_user import run_as_admin
 from src.edit_config import select_language
 from src.edit_config import select_precentage_pass
 from src.edit_config import select_terminal_focus
+from src.edit_config import select_cm_icon
 from src.regkey import write_key
 from src.config import get
 
@@ -21,7 +22,7 @@ def menu(menu_option: int) -> str:
     elif menu_option == 3:
         select_terminal_focus()
     elif menu_option == 4:
-        print("Not yet implemented")
+        select_cm_icon()
     elif menu_option == 0:
         return "Exit"
     elif menu_option is not range(0, 4):
@@ -34,10 +35,11 @@ def main() -> None:
     focus = get("terminal_focus")
     icon = get("cm_icon")
 
-    a = f"1. Set language                        current is {language}, {lang_abbr}\n"
-    b = f"2. Set search strictness               current is {precentage} out of 100%\n"
-    c = f"3. Set Terminal focus on search        current is {focus}\n"
-    d = f"4. Set context_menu icon on/off        current is {icon}\n"
+    a = f"                       --- MENU ---                                  "
+    a = f"1. Change language                current: {language}, {lang_abbr}\n"
+    b = f"2. Set search strictness          current: {precentage} out of 100%\n"
+    c = f"3. Show Terminal on search        current: {focus}\n"
+    d = f"4. Show context menu              current: {icon}\n"
     z = f"\nCtrl+C to exit\n"
     menu_options = [a, b, c, d, z]
 
