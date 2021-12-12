3a5b1bcd7c0d7ceed11c8a4966b7916e5cc78a6b
vagabondHustler <vagabondhustler.github@gmail.com>
2021-12-12 23:48:29 +0100

    Added menu option to run in cmd, ps, pwsh

--- subscene-search/configure.py
+++ subscene-search/configure.py
@@ -1,10 +1,13 @@
 from src.current_user import is_admin
+from src.current_user import got_key
 from src.current_user import run_as_admin
 from src.edit_config import select_language
 from src.edit_config import select_precentage_pass
 from src.edit_config import select_terminal_focus
+from src.edit_config import select_terminal_in
 from src.edit_config import select_cm_icon
 from src.config import get
+from src import registry
 
 
 def menu(menu_option: int) -> str:
@@ -15,7 +18,12 @@ def menu(menu_option: int) -> str:
     elif menu_option == 3:
         select_terminal_focus()
     elif menu_option == 4:
+        select_terminal_in()
+        registry.write_command_subkey()
+
+    elif menu_option == 5:
         select_cm_icon()
+        registry.context_menu_icon()
     elif menu_option == 0:
         return "Exit"
     elif menu_option is not range(0, 4):
@@ -26,15 +34,17 @@ def main() -> None:
     language, lang_abbr = get("language")
     precentage = get("percentage")
     focus = get("terminal_focus")
+    tin = get("terminal_in")
     icon = get("cm_icon")
 
     a = f"                       --- MENU ---                              \n"
     b = f"1. Change language                current: {language}, {lang_abbr}"
     c = f"2. Set search strictness          current: {precentage} out of 100%"
     d = f"3. Show Terminal on search        current: {focus}"
-    e = f"4. Show context menu              current: {icon}"
+    e = f"4. Use cmd, pwsh(>=7) or ps       current: {tin}"
+    f = f"5. Show context menu              current: {icon}"
     z = f"\nCtrl+C to exit\n"
-    menu_options = [a, b, c, d, e, z]
+    menu_options = [a, b, c, d, e, f, z]
 
     for item in menu_options:
         print(item)
@@ -51,5 +61,7 @@ def main() -> None:
 
 if is_admin():
     main()
-else:
+elif got_key:
     run_as_admin()
+else:
+    print("Please run main.py first")
