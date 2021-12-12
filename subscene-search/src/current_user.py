1a03f5ae7ffa9a7c6070a22c11669a3ca2d7e6be
vagabondHustler <vagabondhustler.github@gmail.com>
2021-12-12 23:46:01 +0100

    Removed unused code

--- subscene-search/src/current_user.py
+++ subscene-search/src/current_user.py
@@ -17,13 +17,7 @@ def is_admin() -> bool:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except PermissionError:
         return False
-    
+
+
 def run_as_admin() -> None:
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-
-
-# def got_file() -> bool:
-#     if os.path.isfile(frp("language")) and os.stat(frp("language")).st_size != 0:
-#         return True
-#     else:
-#         return False
