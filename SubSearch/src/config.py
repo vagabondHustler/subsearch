a5d3cce955c8ca3826ce888371a67f1e99e5ce41
vagabondHustler <vagabondhustler.github@gmail.com>
2021-12-22 00:12:52 +0100

    Changed Subscene search to SubSearch

---
+++ SubSearch/src/config.py
@@ -0,0 +1,51 @@
+import json
+from src.sos import root_directory_file
+from src.data import read_data
+
+# get said value(s) from config.json
+def get(output):
+    config_json = read_data(root_directory_file("data/config.json"))
+
+    def language() -> str:
+        language = config_json.language
+        lang, lang_abbr = language.split(", ")
+        return lang, lang_abbr
+
+    def languages() -> list:
+        ls = config_json.languages
+        return ls
+
+    def video_ext() -> list:
+        voe = config_json.video_ext
+        return voe
+
+    def precent() -> int:
+        p = config_json.precentage_pass
+        return p
+
+    def terminal_focus() -> str:
+        tf = config_json.terminal_focus
+        return tf
+
+    def hearing_impaired() -> str:
+        hi = config_json.hearing_impaired
+        return hi
+
+    def cm_icon() -> str:
+        cm = config_json.context_menu_icon
+        return cm
+
+    if output == "language":
+        return language()
+    if output == "languages":
+        return languages()
+    if output == "video_ext":
+        return video_ext()
+    if output == "percentage":
+        return precent()
+    if output == "terminal_focus":
+        return terminal_focus()
+    if output == "hearing_impaired":
+        return hearing_impaired()
+    if output == "cm_icon":
+        return cm_icon()
