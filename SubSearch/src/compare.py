d3e2767ceaf2834455d0655754c817fb48d447de
vagabondHustler <vagabondhustler.github@gmail.com>
2022-05-26 01:30:53 +0200

    Rewrote def check(), less code

--- SubSearch/src/compare.py
+++ SubSearch/src/compare.py
@@ -6,26 +6,16 @@ class Precentage:
     precentage: int
 
 
-# compare two strings with each other and return match in %
-def check(searched: str or int, against: str or int) -> Precentage:
-    match = 0
-    searched: list = mk_lst(searched)
-    against: list = mk_lst(against)
-    greater_answer = is_bigger(searched, against)
-
-    if len(searched) < len(against):
-        for _ga in range(greater_answer):
-            searched.append("_None")
-    elif len(searched) > len(against):
-        for _ga in range(greater_answer):
-            against.append("_None")
-
-    for item_x, item_y in zip(searched, against):
-        if compare(item_x, item_y):
-            match += 1
-
-    tot = len(searched)
-    precentage = round((match / tot) * 100)
+# compare two lists  with each other and return match in %
+def check(from_pc: str or int, from_browser: str or int) -> Precentage:
+    max_precentage = 100
+    pc_list: list = mk_lst(from_pc)
+    browser_list: list = mk_lst(from_browser)
+    not_matching = list(set(pc_list) - set(browser_list))
+    not_matching_value = len(not_matching)
+    number_of_items = len(pc_list)
+    precentage_to_remove = round(not_matching_value / number_of_items * max_precentage)
+    precentage = round(max_precentage - precentage_to_remove)
 
     return Precentage(precentage)
 
@@ -39,22 +29,38 @@ def mk_lst(release: str or int) -> list:
 
     for item in temp:
         if item not in qualities:
-            new.append(item)
+            new.append(item.lower())
     return new
 
 
 # check if strings are equal length
-def is_bigger(searched: str or int, against: str or int) -> int:
-    if len(searched) > len(against):
-        answer = len(searched) - len(against)
+def check_size_difference(x: list, y: list) -> int:
+    if len(x) > len(y):
+        answer = len(x) - len(y)
         return answer
-    elif len(searched) < len(against):
-        answer = len(against) - len(searched)
+    elif len(x) < len(y):
+        answer = len(y) - len(x)
         return answer
+
+
+def make_list_iterable(x: list, y: list, size_difference: int):
+    if len(x) < len(y):
+        for _isb_answer in range(size_difference):
+            x.append("_None")
+    elif len(x) > len(y):
+        for _isb_answer in range(size_difference):
+            y.append("_None")
+    return x, y
 
 
 # compare two items
-def compare(itemx: str or int, itemy: str or int) -> bool:
-    if itemx.lower() == itemy.lower():
+def compare(x: str or int, y: str or int) -> bool:
+    if x.lower() == y.lower():
         return True
     return False
+
+
+def calculate_item_value(x: int, max_value=100) -> int:
+    number_of_items = x
+    value_of_item = max_value / number_of_items
+    return value_of_item
