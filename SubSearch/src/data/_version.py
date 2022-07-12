3ec538ae8799dc85a889fd8f610c8abeadebadb3
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-12 16:11:44 +0200

    Moved from /data to /src/data

---
+++ SubSearch/src/data/_version.py
@@ -0,0 +1,20 @@
+_VERSION = "1.3.6"
+
+
+def current_version(version: str = _VERSION) -> str:
+    """Summary
+
+    Patch release (0.0.1 -> 0.0.2): bug fixes
+    Minor release (0.1.0 -> 0.2.0): larger bug fixes or new features
+    Major release (1.0.0 -> 2.0.0): stable releases
+
+    Args:
+        version (str, optional): _description_. Defaults to _VERSION.
+
+    Returns:
+        str: version
+    """
+    return version
+
+
+current_version()
