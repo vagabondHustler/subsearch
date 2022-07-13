ca83a242a60552da455e93585dbd1d873d5d0d85
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-13 17:26:59 +0200

    Added imports

---
+++ SubSearch/src/gui/settings_menu.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import tkinter as tk
+import webbrowser
+
+import src.utilities.edit_registry as edit_registry
+from src.data._version import current_version
+from src.utilities.current_user import got_key, is_admin, run_as_admin
+from src.utilities.edit_config import set_default_values, update_json
+from src.utilities.fetch_config import get
+from src.utilities.updates import check_for_updates
