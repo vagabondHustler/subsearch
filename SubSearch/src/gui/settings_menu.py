632285d03f932ee04c47e8ba399e4880089ccca3
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-13 17:27:54 +0200

    Global vars

--- SubSearch/src/gui/settings_menu.py
+++ SubSearch/src/gui/settings_menu.py
@@ -17,3 +17,41 @@ if is_admin():
 elif got_key:
     run_as_admin()
     exit()
+
+WINDOW_WIDTH = 560
+WINDOW_HEIGHT = 530
+TBG = "#1b1d22"
+TSLBG = "#4c4c4c"
+TFG = "#bdbdbd"
+TBC = "#121417"
+TEC = "#494B52"
+TFONT10 = "Cascadia 8 bold"
+TFONT20 = "Cascadia 20 bold"
+TLANGUAGE = (
+    "Arabic, ar",
+    "Brazillian Portuguese, pt_BR",
+    "Danish, dk",
+    "Dutch, nl",
+    "English, en",
+    "Finnish, fi",
+    "French, fr",
+    "German, de",
+    "Hebrew, he",
+    "Indonesian, id",
+    "Italian, it",
+    "Korean, ko",
+    "Norwegian, no",
+    "Romanian, ro",
+    "Spanish, es",
+    "Swedish, sv",
+    "Thai, th",
+    "Turkish, tr",
+    "Vietnamese, vi",
+)
+
+
+language, lang_abbr = get("language")
+precentage = get("percentage")
+terminal_focus = get("terminal_focus")
+hearing_impared = get("hearing_impaired")
+cm_icon = get("cm_icon")
