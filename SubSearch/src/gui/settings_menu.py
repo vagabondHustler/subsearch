05935409db6b52109ac944a982693e71c1594be1
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-13 17:27:28 +0200

    Check to see if its ran as admin

--- SubSearch/src/gui/settings_menu.py
+++ SubSearch/src/gui/settings_menu.py
@@ -9,3 +9,11 @@ from src.utilities.current_user import got_key, is_admin, run_as_admin
 from src.utilities.edit_config import set_default_values, update_json
 from src.utilities.fetch_config import get
 from src.utilities.updates import check_for_updates
+
+if is_admin():
+    if got_key() is False:
+        set_default_values()
+        edit_registry.add_context_menu()
+elif got_key:
+    run_as_admin()
+    exit()
