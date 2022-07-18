b852b95ade035d22e7dfb8cf9d3bc1ba6c11f434
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-18 17:36:13 +0200

    Added is_exe_version

--- SubSearch/src/utilities/edit_registry.py
+++ SubSearch/src/utilities/edit_registry.py
@@ -3,8 +3,8 @@ import socket
 import sys
 import winreg
 
-from src.utilities.local_paths import root_directory, root_directory
-from src.utilities.current_user import is_admin, run_as_admin
+from src.utilities.local_paths import root_directory
+from src.utilities.current_user import is_admin, run_as_admin, is_exe_version
 
 COMPUTER_NAME = socket.gethostname()
 
@@ -30,21 +30,28 @@ def write_command_subkey() -> None:
     focus = get("terminal_focus")
 
     command_path = "Directory\Background\shell\SubSearch\command"
+    if is_exe_version():
+        exe_path = root_directory(file_name="SubSearch.exe")
+    else:
+        ppath = os.path.dirname(sys.executable)
+        set_title = "import ctypes; ctypes.windll.kernel32.SetConsoleTitleW('SubSearch');"
+        set_wd = f"import os; working_path = os.getcwd(); os.chdir('{root_directory()}');"
+        run_main = "import main; os.chdir(working_path); main.main()"
 
-    ppath = os.path.dirname(sys.executable)
-    set_title = "import ctypes; ctypes.windll.kernel32.SetConsoleTitleW('SubSearch');"
-    set_wd = f"import os; working_path = os.getcwd(); os.chdir('{root_directory()}');"
-    run_main = "import main; os.chdir(working_path); main.main()"
-
-    tfocus = f'{ppath}\python.exe -c "{set_title} {set_wd} {run_main}"'
-    tsilent = f'{ppath}\pythonw.exe -c "{set_title} {set_wd} {run_main}"'
+        tfocus = f'{ppath}\python.exe -c "{set_title} {set_wd} {run_main}"'
+        tsilent = f'{ppath}\pythonw.exe -c "{set_title} {set_wd} {run_main}"'
 
     with winreg.ConnectRegistry(COMPUTER_NAME, winreg.HKEY_CLASSES_ROOT) as hkey:
         with winreg.OpenKey(hkey, command_path, 0, winreg.KEY_ALL_ACCESS) as subkey_command:
+            if is_exe_version():
+                winreg.SetValueEx(subkey_command, "", 0, winreg.REG_SZ, exe_path)
+                return
             if focus == "True":
                 winreg.SetValueEx(subkey_command, "", 0, winreg.REG_SZ, tfocus)
-            elif focus == "False":
+                return
+            if focus == "False":
                 winreg.SetValueEx(subkey_command, "", 0, winreg.REG_SZ, tsilent)
+                return
 
 
 def restore_context_menu() -> None:
