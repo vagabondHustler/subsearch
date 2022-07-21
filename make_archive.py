23da8562d4cfd7bdb9b700e0bb0ddb80ee363f80
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-21 03:49:12 +0200

    Fixed 'Error: Process completed with exit code 1.'

--- make_archive.py
+++ make_archive.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 
-path_src = f"{os.getcwd()}/app"
-zip_dst = f"{os.getcwd()}/app.zip"
+path_src = f"{os.getcwd()}/app_release/SubSearch"
+zip_dst = f"{os.getcwd()}/SubSearch.zip"
 
 
 def make_archive(source, destination):
