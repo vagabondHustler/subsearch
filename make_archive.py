735bdbe9040c1cd27e91c236c33123f3ccf4093b
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-22 16:07:00 +0200

    Added docstrings

--- make_archive.py
+++ make_archive.py
@@ -1,11 +1,21 @@
 import os
 import shutil
 
+"""
+Used by publish.yml in .github\workflows to create a zip file of the pyinstaller output
+"""
+
 path_x64_dir_src = f"{os.getcwd()}/SubSearch-x64/SubSearch"
 path_x64_zip_dst = f"{os.getcwd()}/SubSearch-x64.zip"
 
 
-def make_archive(source, destination):
+def make_archive(source: str, destination: str) -> None:
+    """
+    Adds folder and file contents to a zip file
+
+    :param str source: Source folder and file contents to be added into a zip file
+    :param str destination: Destination of the zip file
+    """
     base = os.path.basename(destination)
     name = base.split(".")[0]
     format = base.split(".")[1]
