a4dedd705781f2431013041c247ce731dd3998e8
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-23 21:56:32 +0200

    Changed how year is identified
    If the name of the movie/show is
    - "2001: A Foo Bar" from 1991
    
    Before 2001 would be counted as the release year.
    The for loop would break if a number of lenght 4 was detected
    
    Now 1991 would be detected cus the for loop goes trough the list in reverse

--- SubSearch/src/utilities/fetch_parameters.py
+++ SubSearch/src/utilities/fetch_parameters.py
@@ -45,56 +45,68 @@ class SearchParameters:
     file_hash: str
 
 
-def split_last_hyphen(character: str, string: str) -> str:
-    group = string.rsplit(character, 1)[-1]
+def split_last_hyphen(string: str) -> str:
+    group = string.rsplit("-", 1)[-1]
     return group
 
 
-# get all the parameters needed to scrape, from file or directory name
-def get_parameters(
-    directory_path: str, language_abbr: str, file_hash: str, video_release_name: str = None
-) -> SearchParameters:
-    _tmp: list = []
-    if video_release_name is None:
-        directory_name = directory_path.split("\\")
-        # release and group
-        release = directory_name[-1]
-    elif video_release_name is not None:
-        release = video_release_name.replace(" ", ".")
-    if "-" in release:
-        group = split_last_hyphen("-", release)
-    else:
-        group = "N/A"
-    items = release.split(".")
-    # url, title, season, and year
-    for item in items:
-        if item[0].isdigit() and len(item) == 4:
-            year = item
+def find_title_year(x: str, year_found: bool = False) -> tuple:
+    string_list = x.split(".")
+    subtract = []
+    # list is reversed if a year is apart of the title, like "2001: A Foo Bar" from 1991
+    # this is to prevent the year from the title being used as the release year of the movie/show
+    for item in string_list[::-1]:
+        if year_found:
+            _title = (i for i in string_list if i not in subtract)
             break
-        elif item.startswith("s") and item[-1].isdigit():
+        if item.isdigit() and len(item) == 4:
+            year_found = True
+            year = item
+        subtract.append(item)
+
+    title = " ".join(x for x in _title)
+    return title, year
+
+
+def find_season_episode(string: str, lang_abbr: str) -> tuple:
+    season, episode, season_ordinal, episode_ordinal, tv_series = "N/A", "N/A", "N/A", "N/A", False
+    for item in string.lower().split("."):
+        if item.startswith("s") and item[-1].isdigit():
             _season_episode = item.replace("s", "").replace("e", " ")
             season_episode = _season_episode.split(" ")
             sint, eint = season_episode[0], season_episode[1]
             season, episode = f"s{sint}", f"e{eint}"
             season_ordinal, episode_ordinal = (
-                num2words(sint, lang=language_abbr, to="ordinal"),
-                num2words(eint, lang=language_abbr, to="ordinal"),
+                num2words(sint, lang=lang_abbr, to="ordinal"),
+                num2words(eint, lang=lang_abbr, to="ordinal"),
             )
             if season[-1].isdigit():
                 tv_series = True
                 break
-        else:
-            season, episode, season_ordinal, episode_ordinal = "N/A", "N/A", "N/A", "N/A"
-            tv_series = False
-            _tmp.append(item)
-            title = " ".join(_tmp)
-            url_subscene = f"https://subscene.com/subtitles/searchbytitle?query={title}".replace(
-                " ", "%20"
-            )
-            url_opensubtitles = (
-                f"https://www.opensubtitles.org/en/search/sublanguageid-all/moviehash-{file_hash}"
-            )
-            year = "N/A"
+    return season, episode, season_ordinal, episode_ordinal, tv_series
+
+
+def get_parameters(dir_path, lang_abbr, file_hash, file_name) -> SearchParameters:
+    # if no file is found use the current directory path
+    if file_name is None:
+        release = dir_path.split("\\")[-1]
+    else:
+        release = file_name
+
+    title, year = find_title_year(release)
+    i = find_season_episode(release, lang_abbr)
+    season, episode, season_ordinal, episode_ordinal, tv_series = i
+
+    if "-" in release:
+        _group = split_last_hyphen(release)
+        group = _group[-1]
+    else:
+        group = "N/A"
+
+    subscene = "https://subscene.com/subtitles/searchbytitle?query="
+    opensubtitles = "https://www.opensubtitles.org/en/search/sublanguageid-eng/moviename-"
+    url_subscene = f"{subscene}{title}".replace(" ", "%20")
+    url_opensubtitles = f"{opensubtitles}{file_hash}"
 
     parameters = {
         "url_subscene": url_subscene,
