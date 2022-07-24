a7c4ad6d3d52321701c162d1371f72adeb740e34
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-24 15:19:06 +0200

    Add import Hovertip

--- SubSearch/src/gui/download_window.py
+++ SubSearch/src/gui/download_window.py
@@ -3,6 +3,7 @@ import sys
 import tkinter as tk
 from dataclasses import dataclass
 
+from src.gui.tooltip import Hovertip
 from src.scraper.subscene_soup import get_download_url
 from src.utilities.file_manager import (clean_up, download_zip_auto,
                                         extract_zips)
