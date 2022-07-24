12f97f324920e5452c7c3f7e883b20ca88d4d642
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-24 15:03:57 +0200

    Add Tks, dataclass with often used values

--- SubSearch/src/gui/download_window.py
+++ SubSearch/src/gui/download_window.py
@@ -9,3 +9,28 @@ from src.utilities.file_manager import (clean_up, download_zip_auto,
                                         extract_zips)
 from src.utilities.local_paths import cwd, root_directory
 from src.utilities.version import current_version
+
+
+@dataclass
+class Tks:
+    """
+    Dataclass with often used values for the graphical user interface
+    """
+
+    window_width: int = 760
+    window_height: int = 760
+    bg: str = "#1b1d22"
+    bgl: str = "#4c4c4c"
+    fg: str = "#bdbdbd"
+    fge: str = "#c5895e"
+    bc: str = "#121417"
+    abg: str = "#16181c"
+    abg_disabled: str = "#303030"
+    font8: str = "Cascadia 8"
+    font8b: str = "Cascadia 8 bold"
+    font10b: str = "Cascadia 8 bold"
+    font10b: str = "Cascadia 10 bold"
+    font20b: str = "Cascadia 20 bold"
+    col58: str = " " * 58
+
+
