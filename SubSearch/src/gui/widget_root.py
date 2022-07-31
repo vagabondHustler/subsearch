2637ea906440a98a44f0208afa199ae3877354f2
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-31 12:46:38 +0200

    Change imports
    Imported functions:
    - now easier to understand where they come from

--- SubSearch/src/gui/widget_root.py
+++ SubSearch/src/gui/widget_root.py
@@ -1,30 +1,25 @@
 import tkinter as tk
 
-from src.gui.functions import CustomBorder, CustomTitleBar, WindowPosition
-from src.gui.data import Window, Color
-from src.utilities.local_paths import root_directory
-from src.utilities.version import current_version
-
-
-CURRENT_VERSION = current_version()
+from src.gui import tools, tkinter_data as tkd
+from src.utilities import local_paths
 
 
 def main():
     global root
     root = tk.Tk(className=f" SubSearch")
-    icon_path = root_directory("data", "16.ico")
+    icon_path = local_paths.root_directory("data", "16.ico")
     root.iconbitmap(icon_path)
-    root.geometry(WindowPosition.set(root))
+    root.geometry(tools.WindowPosition.set(root))
     root.resizable(False, False)
-    root.wm_attributes("-transparentcolor", Color.grey)
-    root.configure(bg=Color.dark_grey)
-    cb = CustomBorder(root)
+    root.wm_attributes("-transparentcolor", tkd.Color.grey)
+    root.configure(bg=tkd.Color.dark_grey)
+    cb = tools.CustomBorder(root)
     cb.place(relx=0.5, rely=0.5, anchor="center")
-    ctb = CustomTitleBar(root)
-    ctb.place(x=Window.width - 2, y=2, bordermode="inside", anchor="ne")
-    tk.Frame(root, bg=Color.dark_grey).pack(anchor="center", expand=True)
-    tk.Frame(root, bg=Color.dark_grey).pack(anchor="center", expand=True)
-    tk.Frame(root, bg=Color.dark_grey).pack(anchor="center", expand=True)
+    ctb = tools.CustomTitleBar(root)
+    ctb.place(x=tkd.Window.width - 2, y=2, bordermode="inside", anchor="ne")
+    tk.Frame(root, bg=tkd.Color.dark_grey).pack(anchor="center", expand=True)
+    tk.Frame(root, bg=tkd.Color.dark_grey).pack(anchor="center", expand=True)
+    tk.Frame(root, bg=tkd.Color.dark_grey).pack(anchor="center", expand=True)
     ctb.lift()
     cb.lift()
 
