3e44b3f444edce94cc4a96cb370dbf6d97807870
vagabondHustler <vagabondHustler.github@gmail.com>
2022-07-31 12:45:59 +0200

    Fix/Change error out of range, imports
    - tmp.txt is not created, gui won't run
    - imported functions now easier to understand where they come from

--- SubSearch/src/gui/widget_download.py
+++ SubSearch/src/gui/widget_download.py
@@ -2,20 +2,19 @@ import os
 import sys
 import tkinter as tk
 from tkinter import ttk
+import sys
 
 import sv_ttk
-from src.gui.data import Window, Color, Font
-from src.gui.widget_root import main
-from src.scraper.subscene_soup import get_download_url
-from src.utilities.file_manager import clean_up, download_zip_auto, extract_zips
-from src.utilities.local_paths import cwd
+from src.gui import tkinter_data as tkd
+from src.gui import widget_root
+from src.scraper import subscene_soup
+from src.utilities import file_manager, local_paths
 
 
 # file with subtitles and corresponding dl links
 def read_tmp_file(file: str):
-    if os.path.exists(file):
-        with open(file, "r") as f:
-            return [line.strip() for line in f]
+    with open(file, "r") as f:
+        return [line.strip() for line in f]
 
 
 # download said subtitle to the folder with the video file in it
@@ -24,14 +23,13 @@ class DownloadList(tk.Frame):
         tk.Frame.__init__(self, parent)
         # listbox for the subtitles
         self.extent = 0
-        sublist = read_tmp_file("tmp.txt")
-        self.sublist_lenght = len(sublist)
+        self.sublist_lenght = len(SUBLIST)
         self.hs = ttk.Scrollbar(root, orient="vertical", style="Vertical.TScrollbar")
         sub_listbox = tk.Listbox(
             root,
-            bg=Color.dark_grey,
-            fg=Color.light_grey,
-            font=Font.cas8b,
+            bg=tkd.Color.dark_grey,
+            fg=tkd.Color.light_grey,
+            font=tkd.Font.cas8b,
             bd=0,
             border=0,
             borderwidth=0,
@@ -39,8 +37,8 @@ class DownloadList(tk.Frame):
             yscrollcommand=self.hs.set,
         )
         sub_listbox.place(
-            height=Window.height - 60,
-            width=Window.width - 20,
+            height=tkd.Window.height - 60,
+            width=tkd.Window.width - 20,
             relx=0.5,
             rely=0.525,
             bordermode="inside",
@@ -48,7 +46,7 @@ class DownloadList(tk.Frame):
         )
         self.count = 0
         self.sub_listbox = sub_listbox
-        self.sublist = sublist
+        self.sublist = SUBLIST
         self.fill_listbox()
         # custom scrollbar
         scrollbar_lengt = 50
@@ -68,7 +66,7 @@ class DownloadList(tk.Frame):
             arrowsize=24,
         )
 
-        self.hs.place(x=Window.width - 28, y=51, bordermode="inside", height=633)
+        self.hs.place(x=tkd.Window.width - 28, y=51, bordermode="inside", height=633)
         self.hs.config(command=self.sub_listbox.yview)
         self.hs.lift()
 
@@ -108,33 +106,35 @@ class DownloadList(tk.Frame):
             if number == int(items):
                 self.sub_listbox.delete(int(number))
                 self.sub_listbox.insert(int(number), f"»»» DOWNLOADING «««")
-                self.sub_listbox.itemconfig(int(number), {"fg": Color.blue})
+                self.sub_listbox.itemconfig(int(number), {"fg": tkd.Color.blue})
                 try:
-                    dl_url = get_download_url(url)
+                    dl_url = subscene_soup.get_download_url(url)
                     _name = name.replace("/", "").replace("\\", "").split(": ")
-                    path = f"{cwd()}\\{_name[-1]}.zip"
+                    path = f"{local_paths.cwd()}\\{_name[-1]}.zip"
                     item = path, dl_url, 1, 1
-                    download_zip_auto(item)
+                    file_manager.download_zip_auto(item)
                     if _error is False:
-                        extract_zips(cwd(), ".zip")
-                        clean_up(cwd(), ".zip")
-                        clean_up(cwd(), ").nfo")
+                        file_manager.extract_zips(local_paths.cwd(), ".zip")
+                        file_manager.clean_up(local_paths.cwd(), ".zip")
+                        file_manager.clean_up(local_paths.cwd(), ").nfo")
                         self.sub_listbox.delete(int(number))
                         self.sub_listbox.insert(int(number), f"✔ {name}")
-                        self.sub_listbox.itemconfig(int(number), {"fg": Color.green})
+                        self.sub_listbox.itemconfig(int(number), {"fg": tkd.Color.green})
                         _error = False
                 except OSError:
                     _error = True
                     self.sub_listbox.delete(int(number))
                     self.sub_listbox.insert(int(number), f"⚠⚠⚠ Download failed ⚠⚠⚠")
-                    self.sub_listbox.itemconfig(int(number), {"fg": Color.red})
+                    self.sub_listbox.itemconfig(int(number), {"fg": tkd.Color.red})
 
 
-root = main()
-sv_ttk.set_theme("dark")
-DownloadList(root).pack(anchor="center")
-tk.Frame(root, bg=Color.dark_grey).pack(anchor="center", expand=True)
-root.mainloop()
+SUBLIST = read_tmp_file("tmp.txt")
 
 
-sys.exit()
+def show_widget():
+    global root
+    root = widget_root.main()
+    sv_ttk.set_theme("dark")
+    DownloadList(root).pack(anchor="center")
+    tk.Frame(root, bg=tkd.Color.dark_grey).pack(anchor="center", expand=True)
+    root.mainloop()
