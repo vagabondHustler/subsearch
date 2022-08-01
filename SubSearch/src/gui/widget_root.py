fb7869327a3490608dd1ec931d25f84b6a126a42
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-01 13:31:15 +0200

    Change 1 wider tk.frame instead of several small ones

--- SubSearch/src/gui/widget_root.py
+++ SubSearch/src/gui/widget_root.py
@@ -13,14 +13,8 @@ def main():
     root.resizable(False, False)
     root.wm_attributes("-transparentcolor", tkd.Color.grey)
     root.configure(bg=tkd.Color.dark_grey)
-    cb = tools.CustomBorder(root)
-    cb.place(relx=0.5, rely=0.5, anchor="center")
-    ctb = tools.CustomTitleBar(root)
-    ctb.place(x=tkd.Window.width - 2, y=2, bordermode="inside", anchor="ne")
-    tk.Frame(root, bg=tkd.Color.dark_grey).pack(anchor="center", expand=True)
-    tk.Frame(root, bg=tkd.Color.dark_grey).pack(anchor="center", expand=True)
-    tk.Frame(root, bg=tkd.Color.dark_grey).pack(anchor="center", expand=True)
-    ctb.lift()
-    cb.lift()
+    tools.CustomBorder(root).place(relx=0.5, rely=0.5, anchor="center")
+    tk.Frame(root, height=40, bg=tkd.Color.dark_grey).pack(anchor="center", expand=True)
+    tools.CustomTitleBar(root).place(x=tkd.Window.width - 2, y=2, bordermode="inside", anchor="ne")
 
     return root
