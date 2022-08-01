92eda65c62be1cd1853265ed14eebd1cccf42e87
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-01 13:29:31 +0200

    Del unused code

--- SubSearch/src/utilities/version.py
+++ SubSearch/src/utilities/version.py
@@ -1,6 +1,6 @@
 import json
 
-from src.utilities import edit_config, local_paths
+from src.utilities import local_paths
 
 
 def read_data(config_file: str):
@@ -13,43 +13,3 @@ def read_data(config_file: str):
 def current():
     c_version = read_data(local_paths.root_directory("data", "version.json"))
     return c_version
-
-
-version_on_load = current()
-
-
-def increase_version(current_version: str, release_type: str, i: int = 1):
-    """
-    Increase the version number
-
-    :param str current_version: The current version number
-    :param str release_type: (major, minor, or patch)
-    :return str: _description_
-    """
-    c_version = current_version.replace("v", "")
-    major, minor, patch = c_version.split(".")
-    if release_type.lower() == "patch":
-        patch = int(patch) + i
-    if release_type.lower() == "minor":
-        minor = int(minor) + i
-    if release_type.lower() == "major":
-        major = int(major) + i
-    else:
-        pass
-
-    return f"v{major}.{minor}.{patch}"
-
-
-def add_patch_minor_major(release_type: str):
-    """
-    Patch release (0.0.1 -> 0.0.2): bug fixes
-    Minor release (0.1.0 -> 0.2.0): larger bug fixes or new features
-    Major release (1.0.0 -> 2.0.0): stable releases
-
-    :param str release_type: "major", "minor", "patch", defaults to sys.argv[1]
-    """
-    print(f"Current Version: {current()}")
-    new_version = increase_version(current(), release_type)
-    edit_config.update_json("version", new_version, "data", "version.json")
-
-    print(f"New Version: {new_version}, Old Version: {version_on_load}")
