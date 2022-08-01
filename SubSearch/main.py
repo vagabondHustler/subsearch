211cd8e1f575fb9e66c2f51412900bcf192c07f6
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-01 13:30:06 +0200

    Del unused code

--- SubSearch/main.py
+++ SubSearch/main.py
@@ -1,29 +1,18 @@
 import subprocess
 import sys
 
-from src.utilities import current_user, local_paths, read_config_json
+from src.utilities import current_user, local_paths, read_config
 
 
 def main():
-    try:
-        release_type = "major", "minor", "patch"
-        if sys.argv[1] in release_type:
-            from src.utilities.version import add_patch_minor_major
-
-            add_patch_minor_major(sys.argv[1])
-            return
-    except IndexError:
-        pass
-    
+    if current_user.is_exe() is False:
+        show_terminal = read_config.get("show_terminal")
+        if str(sys.executable).endswith("python.exe") and show_terminal == "False":
+            return subprocess.Popen(["pythonw", "main.py"])
     if local_paths.cwd() == local_paths.root_directory():
-        # hides terminal window
-        tf = read_config_json.get("terminal_focus")
-        if str(sys.executable).endswith("python.exe") and tf == "False":
-            if current_user.is_exe() is False:
-                return subprocess.Popen(["pythonw", "main.py"])
-        else:
-            from src.gui import widget_settings
-            widget_settings.show_widget()
+        from src.gui import widget_settings
+
+        widget_settings.show_widget()
 
     elif local_paths.cwd() != local_paths.root_directory():
         import src.subsearch
