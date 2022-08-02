6ec98298fef6e5719e598ba7935328f0697dc9e5
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-02 21:53:29 +0200

    Update if...if.. change to dict for better scaling

--- SubSearch/src/utilities/local_paths.py
+++ SubSearch/src/utilities/local_paths.py
@@ -4,22 +4,38 @@ import os
 def cwd():
     return os.getcwd()
 
+
 # get root directory path
-def root_directory(directory: str = "src", file_name: str = None):
+def paths(file_name: str = None):
     path, _i = os.path.split(os.path.abspath(__file__))
-    if directory == "src":
-        path = path.replace(r"\src\utilities", "")
-    if directory == "data":
-        path = path.replace(r"\src\utilities", "\src\data")
-    if directory == "gui":
-        path = path.replace(r"\src\utilities", "\src\gui")
-    if directory == "scraper":
-        path = path.replace(r"\src\utilities", "\src\scraper")
-    if directory == "utilities":
-        pass
-    if file_name is not None:
-        path = add_file_to_path(path, file_name)
-    return path
+    if file_name is None:
+        file = ""
+    else:
+        file = f"\\{file_name}"
+    paths = {
+        "cwd": cwd(),
+        "root": path.replace("\\src\\utilities", f"{file}"),
+        "src": path.replace("\\src\\utilities", f"\\src{file}"),
+        "data": path.replace("\\src\\utilities", f"\\src\\data{file}"),
+        "gui": path.replace("\\src\\utilities", f"\\src\\gui{file}"),
+        "scraper": path.replace("\\src\\utilities", f"\\src\\scraper{file}"),
+        "icons": path.replace("\\src\\utilities", f"\\src\\gui\\assets\\icons{file}"),
+        "buttons": path.replace("\\src\\utilities", f"\\src\\gui\\assets\\buttons{file}"),
+        "utilities": path,
+    }
+    return paths
+
+def get_path(directory: str, file_name: str = None):
+    """get_path to a directory or directory and file
+
+    Args:
+        directory: src, data, gui, scraper, icons, buttons, utilities
+        file_name: any. Defaults to None.
+
+    Returns:
+        dict[str, str]: path to directory or directory and file
+    """
+    return paths(file_name)[f"{directory}"].lower()
 
 def add_file_to_path(root_dir_path: str, file_name: str):
     file_path = f"{root_dir_path}\{file_name}"
