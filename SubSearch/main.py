03ae4dd25af78c80b1ad8f2598f1e7976c469182
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-02 22:03:52 +0200

    Del unsued code

--- SubSearch/main.py
+++ SubSearch/main.py
@@ -5,16 +5,12 @@ from src.utilities import current_user, local_paths, read_config
 
 
 def main():
-    if current_user.is_exe() is False:
-        show_terminal = read_config.get("show_terminal")
-        if str(sys.executable).endswith("python.exe") and show_terminal == "False":
-            return subprocess.Popen(["pythonw", "main.py"])
-    if local_paths.cwd() == local_paths.root_directory():
+    if local_paths.get_path("cwd") == local_paths.get_path("root"):
         from src.gui import widget_settings
 
         widget_settings.show_widget()
 
-    elif local_paths.cwd() != local_paths.root_directory():
+    elif local_paths.get_path("cwd") != local_paths.get_path("root"):
         import src.subsearch
 
         src.subsearch.main(sys.argv[-1])
@@ -22,3 +18,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
