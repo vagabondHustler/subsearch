bc28e13ba9ef9b04587b8be5b14d8fb94ddb0480
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-05 17:38:27 +0200

    Initial commit:: update version in __version__.py /data

---
+++ .github/workflows/_set__version__.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+cwd = os.getcwd()
+
+
+def read_data(config_file: str):
+    with open(config_file, encoding="utf-8") as file:
+        data = json.load(file)
+        version = data["version"]
+        return version
+
+
+def current():
+    c_version = read_data(f"{cwd}/src/subsearch/data/version.json")
+    return c_version
+
+
+v = current().replace("v", "")
+version = f'__version__ = "{v}"'
+
+# Write the file out again
+with open(f"{cwd}/src/subsearch/data/__version__.py", "w") as file:
+    file.write(str(version))
