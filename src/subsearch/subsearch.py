8dbe47d7f753c1dda4cc076b2413300992d41e78
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-06 15:32:40 +0200

    Update: for build 2.13.29

--- src/subsearch/subsearch.py
+++ src/subsearch/subsearch.py
@@ -4,12 +4,16 @@ from util import search, local_paths, current_user, raw_config, raw_registry
 from gui import widget_settings
 
 
-def main() -> None:
+def main(input: str = None) -> None:
+    """
+    Args:
+        input: --settings
+    """
     if current_user.got_key() is False:
         raw_config.set_default_json()
         raw_registry.add_context_menu()
 
-    if local_paths.get_path("cwd") == local_paths.get_path("root") or sys.argv[-1].endswith(".py"):
+    if local_paths.get_path("cwd") == local_paths.get_path("root") or input == "--settings":
         widget_settings.show_widget()
 
     elif local_paths.get_path("cwd") != local_paths.get_path("root") and not sys.argv[-1].endswith(".py"):
