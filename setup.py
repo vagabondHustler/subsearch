886e9c8904ec14c4d4fc3eb531b155a3a057b584
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-06 14:31:05 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-import setuptools
 
-if __name__ == "__main__":
-    setuptools.setup()
+from setuptools import setup
+
+
+def read_file(fname):
+    with open(fname, "r") as f:
+        return f.read()
+
+requirements = read_file("requirements.txt").strip().split()
+
+
+setup(
+    install_requires=requirements,
+    package_data={"": ["requirements.text", "*.png", "*.ico"]},
+)
