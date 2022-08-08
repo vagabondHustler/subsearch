0361eaf255b5f96aaf0a2266e1caa8e0891cc61d
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-08 02:15:22 +0200

    Update: argument input now called enter

--- src/subsearch/main.py
+++ src/subsearch/main.py
@@ -4,7 +4,7 @@ from gui import widget_settings
 from util import current_user, local_paths, raw_config, raw_registry, search
 
 
-def main(input: str = None) -> None:
+def main(enter: str = None) -> None:
     """
     main function for subsearch
 
@@ -12,22 +12,19 @@ def main(input: str = None) -> None:
     ----------
     input : str, optional
         --settings, by default None
+
     """
     if current_user.got_key() is False:
         raw_config.set_default_json()
         raw_registry.add_context_menu()
 
-    if (
-        local_paths.get_path("cwd") == local_paths.get_path("root")
-        or input == "--settings"
-    ):
+    if local_paths.get_path("cwd") == local_paths.get_path("root") or enter == "--settings":
         widget_settings.show_widget()
 
-    elif local_paths.get_path("cwd") != local_paths.get_path("root") and not sys.argv[
-        -1
-    ].endswith(".py"):
+    elif local_paths.get_path("cwd") != local_paths.get_path("root") and not sys.argv[-1].endswith(".py"):
         search.run_search(sys.argv[-1])
 
 
 if __name__ == "__main__":
     main()
+
