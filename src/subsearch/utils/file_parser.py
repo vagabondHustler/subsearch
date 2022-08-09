bc82426b7a16daee89e27b2f3939aabcc65d4a59
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-09 18:08:20 +0200

    Update: how searching for a show works
    The search term used to be:
    <name of the show> sxxexx
    e.g. foobar s03e01
    
    The search term now is:
    <name of the show> <season_ordinal> season
    e.g foobar third season

--- src/subsearch/utils/file_parser.py
+++ src/subsearch/utils/file_parser.py
@@ -24,9 +24,9 @@ def split_last_hyphen(string: str) -> str:
     return group
 
 
-def get_parameters(lang_abbr: str, file_hash: str, file_name: str) -> SearchParameters:
+def get_parameters(file_name: str, file_hash: str, lang_abbr: str) -> SearchParameters:
     # default values
-    _title = None
+    title = "N/A"
     year = "N/A"
     season = "N/A"
     season_ordinal = "N/A"
@@ -62,10 +62,10 @@ def get_parameters(lang_abbr: str, file_hash: str, file_name: str) -> SearchPara
             year = item
         subtract.append(item)
 
-    if _title is not None:
-        title = " ".join(x for x in _title)
+    if tv_series:
+        title = " ".join(x for x in _title).replace(f"s{season}e{episode}", f"- {season_ordinal} season").strip()
     else:
-        title = release
+        title = " ".join(x for x in _title)
 
     if "-" in release:
         group = split_last_hyphen(release)
