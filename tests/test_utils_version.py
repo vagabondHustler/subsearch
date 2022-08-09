ce693baeb2d3c43eed7e5b76eb137f644f333d81
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-09 21:22:02 +0200

    Add: check if version_text = "v", check if there are 2 "."
    To make sure the entire c_version string is numbers, one v and two ..

--- tests/test_utils_version.py
+++ tests/test_utils_version.py
@@ -6,5 +6,9 @@ from src.subsearch.utils import version
 def test_current() -> None:
     c_version = version.current()
     version_digits = "".join(re.findall(r"\d+", c_version))
+    version_text = "".join(re.findall(r"[a-zA-Z]+", c_version))
+    version_dots = "".join(re.findall(r"\.", c_version))
 
     assert version_digits.isnumeric() is True
+    assert version_text.isalpha() is True
+    assert version_dots == ".."
