c12ef333d69971e1f9e3e52fc8538d9419201987
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-09 19:44:51 +0200

    Add: setup files for testing

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+
+def read_file(fname):
+    with open(fname, "r") as f:
+        return f.read()
+
+requirements = read_file("docs/requirements.txt").strip().split()
+
+
+setup(
+    install_requires=requirements,
+    package_data={"": ["requirements.text", "*.png", "*.ico"]},
+)
