04c3e1a5a3a2361ef36969d0edd2b45c570d7346
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-10 22:53:47 +0200

    Update: string names

--- tests/test_utils_string_parser.py
+++ tests/test_utils_string_parser.py
@@ -2,9 +2,11 @@ from src.subsearch.utils import string_parser
 
 
 def test_file_parser_movie() -> None:
-    file_movie = "the.foo.bar.2021.1080p.web.h264-foobar"
-    file_show = "the.foo.bar.s01e01.1080p.web.h264-foobar"
-    file_fail = "then.fooing.baring.2022.720p.webby.h265-f00bar"
+    movie_1080p = "the.foo.bar.2021.1080p.web.h264-foobar"
+    movie_720p = "the.foo.bar.2021.720p.web.h264-foobar"
+    show_1080p = "the.foo.bar.s01e01.1080p.web.h264-foobar"
+    show_720p = "the.foo.bar.s01e01.720p.web.h264-foobar"
+    no_match = "then.fooing.baring.2022.720p.webby.h265-f00bar"
 
     pct0 = string_parser.pct_value(file_movie, file_show)
     pct1 = string_parser.pct_value(file_show, file_movie)
