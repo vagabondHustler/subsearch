007e21407be09c8d3bc2b02553aecff9c779ee99
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-10 22:55:57 +0200

    Add: assert 1080p and 720p has no effect on pct

--- tests/test_utils_string_parser.py
+++ tests/test_utils_string_parser.py
@@ -8,12 +8,16 @@ def test_file_parser_movie() -> None:
     show_720p = "the.foo.bar.s01e01.720p.web.h264-foobar"
     no_match = "then.fooing.baring.2022.720p.webby.h265-f00bar"
 
-    pct0 = string_parser.pct_value(file_movie, file_show)
-    pct1 = string_parser.pct_value(file_show, file_movie)
-    pct2 = string_parser.pct_value(file_movie, file_movie)
-    pct3 = string_parser.pct_value(file_movie, file_fail)
+    pct0 = string_parser.pct_value(movie_1080p, movie_720p)
+    pct1 = string_parser.pct_value(show_1080p, show_720p)
+    pct2 = string_parser.pct_value(movie_1080p, show_1080p)
+    pct3 = string_parser.pct_value(show_1080p, movie_1080p)
+    pct4 = string_parser.pct_value(movie_1080p, movie_1080p)
+    pct5 = string_parser.pct_value(movie_1080p, no_match)
 
-    assert pct0.percentage == 83
-    assert pct1.percentage == 83
-    assert pct2.percentage == 100
-    assert pct3.percentage == 0
+    assert pct0.percentage == 100
+    assert pct1.percentage == 100
+    assert pct2.percentage == 83
+    assert pct3.percentage == 83
+    assert pct4.percentage == 100
+    assert pct5.percentage == 0
