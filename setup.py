c41a3d6398313cc913dc917710bbfc598be5d3bf
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-11 20:55:26 +0200

    Del: install_requires, now only in setup.cfg

--- setup.py
+++ setup.py
@@ -1,15 +1,5 @@
 from setuptools import setup
 
-
-def read_file(fname):
-    with open(fname, "r") as f:
-        return f.read()
-
-
-requirements = read_file("docs/requirements.txt").strip().split()
-
-
 setup(
-    install_requires=requirements,
-    package_data={"": ["requirements.text", "*.png", "*.ico"]},
+    package_data={"": ["requirements.text", "dev_requirements.text", "*.png", "*.ico"]},
 )
