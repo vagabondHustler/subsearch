7cf228865fab60e41b599b9e62cd38efafe3bb26
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-13 00:07:58 +0200

    fix:: broken... no more

--- src/subsearch/subsearch.py
+++ src/subsearch/subsearch.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-from utils import current_user, raw_config, raw_registry
+from gui import widget_settings
+from utils import current_user, raw_config, raw_registry, search
 
 
 def main() -> None:
@@ -15,7 +16,7 @@ def main() -> None:
         raw_config.set_default_json()
         raw_registry.add_context_menu()
     if current_user.check_is_exe():
-        raw_config.set_json("show_terminal", "False")
+        raw_config.set_json("show_terminal", False)
     show_terminal = raw_config.get("show_terminal")
     for ext in raw_config.get("file_ext"):
         if sys.argv[-1].endswith(ext):
@@ -28,16 +29,12 @@ def main() -> None:
             break
 
     if file_exist is False:
-        from gui import widget_settings
-
         widget_settings.show_widget()
 
     if file_exist:
-        from utils import search
-
         search.run_search(file_name_ext, file_path)
 
-    if show_terminal == "True":
+    if show_terminal is True:
         input()
 
 
