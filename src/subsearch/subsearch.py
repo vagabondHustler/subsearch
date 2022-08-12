b78302a0be73a0bd054a9118dfca04723a5f3229
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-12 19:59:19 +0200

    fix:: exe, ran a search when it shouldn't have

--- src/subsearch/subsearch.py
+++ src/subsearch/subsearch.py
@@ -1,33 +1,43 @@
 import os
 import sys
 
-from utils import current_user, local_paths, raw_config, raw_registry, search
+from utils import current_user, raw_config, raw_registry
 
 
 def main() -> None:
     """
     main function of subsearch
     """
+    file_path = ""
+    file_name_ext = ""
+    file_exist = False
+
+    if current_user.got_key() is False:
+        raw_config.set_default_json()
+        raw_registry.add_context_menu()
+
     for ext in raw_config.get("file_ext"):
         if sys.argv[-1].endswith(ext):
+            file_exist = True
             file_full_path = sys.argv[-1]
             index = file_full_path.rfind("\\")
             file_path = file_full_path[0:index]
             file_name_ext = file_full_path[index + 1 :]
             os.chdir(file_path)
+            break
 
-    if current_user.got_key() is False:
-        raw_config.set_default_json()
-        raw_registry.add_context_menu()
-    if local_paths.cwd() == local_paths.get_path("root") or sys.argv[-1].endswith("subsearch.py"):
+    if file_exist is False:
         from gui import widget_settings
 
         widget_settings.show_widget()
-    elif local_paths.cwd() != local_paths.get_path("root"):
+
+    if file_exist:
+        from utils import search
+
         search.run_search(file_name_ext, file_path)
 
     show_terminal = raw_config.get("show_terminal")
-    if show_terminal:
+    if show_terminal and current_user.is_exe() is False:
         input()
 
 
