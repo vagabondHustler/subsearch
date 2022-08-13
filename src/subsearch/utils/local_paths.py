459fde8415d9c36dbb55a58c454eab07fb4beb35
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-13 22:22:18 +0200

    update:: str paths -> _paths, path -> _path, paths - _dict

--- src/subsearch/utils/local_paths.py
+++ src/subsearch/utils/local_paths.py
@@ -7,23 +7,23 @@ def cwd() -> str:
 
 
 # get root directory path
-def paths(file_name: str = None) -> dict:
-    path, _i = os.path.split(os.path.abspath(__file__))
+def _paths(file_name: str = None) -> dict:
+    _path, _i = os.path.split(os.path.abspath(__file__))
     if file_name is None:
         file = ""
     else:
         file = f"\\{file_name}"
-    paths = {
+    _dict = {
         "cwd": cwd(),
-        "root": path.replace("\\utils", f"{file}"),
-        "data": path.replace("\\utils", f"\\data{file}"),
-        "gui": path.replace("\\utils", f"\\gui{file}"),
-        "scraper": path.replace("\\utils", f"\\scraper{file}"),
-        "utils": f"{path}{file}",
-        "icons": path.replace("\\utils", f"\\assets\\icons{file}"),
-        "buttons": path.replace("\\utils", f"\\assets\\buttons{file}"),
+        "root": _path.replace("\\utils", f"{file}"),
+        "data": _path.replace("\\utils", f"\\data{file}"),
+        "gui": _path.replace("\\utils", f"\\gui{file}"),
+        "scraper": _path.replace("\\utils", f"\\scraper{file}"),
+        "utils": f"{_path}{file}",
+        "icons": _path.replace("\\utils", f"\\assets\\icons{file}"),
+        "buttons": _path.replace("\\utils", f"\\assets\\buttons{file}"),
     }
-    return paths
+    return _dict
 
 
 def get_path(directory: str, file_name: str = None) -> str:
@@ -35,4 +35,4 @@ def get_path(directory: str, file_name: str = None) -> str:
     str
         the directory or directory and file
     """
-    return paths(file_name)[f"{directory}"].lower()
+    return _paths(file_name)[f"{directory}"].lower()
