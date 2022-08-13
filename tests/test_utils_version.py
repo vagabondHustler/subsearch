c7b1e2fbb107530a23bbc47cbba6ba0da1a71c6a
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-13 22:19:45 +0200

    update:: test version only uses __version__

--- tests/test_utils_version.py
+++ tests/test_utils_version.py
@@ -1,17 +1,15 @@
 import re
 
-from src.subsearch.utils import version
+from data import __version__
 
 
 def test_current() -> None:
     """
     test so to ensure that the src/subsearch/utils/version.current function returns a with the correct format
     """
-    c_version = version.current()
-    version_digits = "".join(re.findall(r"\d+", c_version))
-    version_text = "".join(re.findall(r"[a-zA-Z]+", c_version))
-    version_dots = "".join(re.findall(r"\.", c_version))
+
+    version_digits = "".join(re.findall(r"\d+", __version__))
+    version_dots = "".join(re.findall(r"\.", __version__))
 
     assert version_digits.isnumeric() is True
-    assert version_text == "v"
     assert version_dots == ".."
