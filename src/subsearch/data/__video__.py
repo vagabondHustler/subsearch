d325bbd347e007e381ced8865fea4c35770a6e42
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-15 03:16:43 +0200

    initial commit:: __video__

---
+++ src/subsearch/data/__video__.py
@@ -0,0 +1,6 @@
+from utils._set_data_ import VideoFile
+
+__video_name__ = getattr(VideoFile, "name")
+__video_ext__ = getattr(VideoFile, "ext")
+__video_path__ = getattr(VideoFile, "path")
+__video_directory__ = getattr(VideoFile, "directory")
