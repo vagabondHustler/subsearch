587f6d90eff2c3d8ecb35cb9e0559ec10c024a29
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-15 03:16:17 +0200

    initial commit:: __paths__

---
+++ src/subsearch/data/__paths__.py
@@ -0,0 +1,9 @@
+from utils._set_data_ import Paths
+
+__root__ = getattr(Paths, "root")
+__data__ = getattr(Paths, "data")
+__gui__ = getattr(Paths, "gui")
+__scraper__ = getattr(Paths, "scraper")
+__utils__ = getattr(Paths, "utils")
+__icons__ = getattr(Paths, "icons")
+__buttons__ = getattr(Paths, "buttons")
