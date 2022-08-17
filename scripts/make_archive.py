35aa577bd9c7155732d4515d73d2f68e756a5c72
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-18 00:19:23 +0200

    update:: code to fit workflow
    update:: paths
    rename:: removed _ at start of each file

--- scripts/make_archive.py
+++ scripts/make_archive.py
@@ -4,15 +4,10 @@ import shutil
 """
 Used by release.yml in .github/workflows to create a zip files of the repository contents
 """
-ss_upper = f"{os.getcwd()}/src/SubSearch"
-ss_lower = f"{os.getcwd()}/src/subsearch"
 
-os.rename(ss_lower, ss_upper)
-path_py_dir_src = f"{os.getcwd()}/src/SubSearch"
-path_py_zip_dst = f"{os.getcwd()}/SubSearch-source.zip"
+
 path_x64_dir_src = f"{os.getcwd()}/SubSearch-x64/SubSearch"
 path_x64_zip_dst = f"{os.getcwd()}/SubSearch-x64.zip"
-os.rename(ss_upper, ss_lower)
 
 
 def make_archive(source: str, destination: str):
