f4b43413238bf33beeb716061d3dbf52c28be5c4
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-18 01:32:43 +0200

    fix:: module not found

--- scripts/set__version__.py
+++ scripts/set__version__.py
@@ -5,7 +5,13 @@ cwd = os.getcwd()
 
 
 v = sys.argv[-1].replace("v", "")
-version = f"""__version__ = "{v}"
+version = f"""__version__ = "2.15.47"
+
+import sys
+
+if sys.argv[-1] == "--get-version":
+    print(__version__)
+
 """
 
 with open(f"{cwd}/src/subsearch/data/__version__.py", "w") as file:
