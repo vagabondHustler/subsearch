87cb22e40a41c7cb224b5e4aeeb06be70889bfba
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-18 00:26:18 +0200

    tests:: imports updated

--- tests/test_utils_string_parser.py
+++ tests/test_utils_string_parser.py
@@ -11,12 +11,12 @@ def test_file_parser_movie() -> None:
     show_720p = "the.foo.bar.s01e01.720p.web.h264-foobar"
     no_match = "then.fooing.baring.2022.720p.webby.h265-f00bar"
 
-    pct0 = string_parser.pct_value(movie_1080p, movie_720p)
-    pct1 = string_parser.pct_value(show_1080p, show_720p)
-    pct2 = string_parser.pct_value(movie_1080p, show_1080p)
-    pct3 = string_parser.pct_value(show_1080p, movie_1080p)
-    pct4 = string_parser.pct_value(movie_1080p, movie_1080p)
-    pct5 = string_parser.pct_value(movie_1080p, no_match)
+    pct0 = string_parser.get_pct_value(movie_1080p, movie_720p)
+    pct1 = string_parser.get_pct_value(show_1080p, show_720p)
+    pct2 = string_parser.get_pct_value(movie_1080p, show_1080p)
+    pct3 = string_parser.get_pct_value(show_1080p, movie_1080p)
+    pct4 = string_parser.get_pct_value(movie_1080p, movie_1080p)
+    pct5 = string_parser.get_pct_value(movie_1080p, no_match)
 
     assert pct0 == 100
     assert pct1 == 100
