194c44296bf060f527fa39f28b1c2347568df308
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-19 04:26:26 +0200

    reformat:: codebase with black & isort

--- tests/test_data_set_data_.py
+++ tests/test_data_set_data_.py
@@ -22,3 +22,4 @@ def test_paths() -> None:
     assert __utils__ == os.path.join(os.getcwd(), "src\\subsearch\\utils")
     assert __icons__ == os.path.join(os.getcwd(), "src\\subsearch\\assets\\icons")
     assert __buttons__ == os.path.join(os.getcwd(), "src\\subsearch\\assets\\buttons")
+
