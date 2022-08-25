8642afd7d5fb6cdbd615072f19a946ce9a46dc81
vagabondHustler <vagabondHustler.github@gmail.com>
2022-08-25 04:49:27 +0200

    Refactor results -> div_title, tda1 -> td_a1

--- src/subsearch/scraper/subscene_soup.py
+++ src/subsearch/scraper/subscene_soup.py
@@ -30,10 +30,10 @@ def search_for_title(url: str) -> (Union[dict[str, str], Literal["ERROR: CAPTCHA
         doc_captcha = doc.find("h2", text="Why do I have to complete a CAPTCHA?")
         if doc_captcha.text == "Why do I have to complete a CAPTCHA?":
             return "ERROR: CAPTCHA PROTECTION"
-    results = doc_result.find_all("div", class_="title")
-    for result in results:
-        title_name = result.contents[1].contents[0].strip().replace(":", "")
-        title_url = result.contents[1].attrs["href"]
+    div_title = doc_result.find_all("div", class_="title")
+    for item in div_title:
+        title_name = item.contents[1].contents[0].strip().replace(":", "")
+        title_url = item.contents[1].attrs["href"]
         titles[title_name] = f"https://subscene.com{title_url}"
     return titles
 
@@ -47,11 +47,11 @@ def search_title_for_sub(language: str, hearing_impaired: Union[bool, str], url:
         doc = BeautifulSoup(scontent, "lxml")
         tbody = doc.find("tbody")
         if tbody is not None:
-            tda1 = tbody.find_all("td", class_="a1")
+            td_a1 = tbody.find_all("td", class_="a1")
             searching = False
         else:
             time.sleep(1)
-    for a1 in tda1:
+    for a1 in td_a1:
         sub_hi = is_sub_hi(a1)
         subtitle_language = a1.contents[1].contents[1].contents[0].strip()
         if hearing_impaired != "Both" and hearing_impaired != sub_hi:
