78f8fa1de424fd8279c365ebe13a8ee0ae748f4e
vagabondHustler <vagabondHustler.github@gmail.com>
2022-09-03 16:31:13 +0200

    Refactor __video__ -> __set_video__

--- src/subsearch/data/__video__.py
+++
@@ -1,6 +0,0 @@
-from .__set_data__ import VideoFile
-
-__video_name__: str = getattr(VideoFile, "name")
-__video_ext__: str = getattr(VideoFile, "ext")
-__video_path__: str = getattr(VideoFile, "path")
-__video_directory__: str = getattr(VideoFile, "directory")
