c48fb69af73cc9de746fa68a731715978e9aa2db
vagabondHustler <vagabondHustler.github@gmail.com>
2022-09-03 16:32:45 +0200

    Feature update __init__ imports

--- src/subsearch/data/__init__.py
+++ src/subsearch/data/__init__.py
@@ -1,19 +1,14 @@
-from .__set_data__ import SetValues
-
-__set_values__ = SetValues()
-from .__paths__ import (
+from .__set_paths__ import (
     __buttons__,
     __data__,
     __gui__,
+    __home__,
     __icons__,
-    __root__,
+    __providers__,
     __tabs__,
     __utils__,
 )
+from .__set_video__ import get_video_file_data
 from .__version__ import __version__
-from .__video__ import (
-    __video_directory__,
-    __video_ext__,
-    __video_name__,
-    __video_path__,
-)
+
+__video__ = get_video_file_data()
