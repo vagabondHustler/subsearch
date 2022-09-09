da7901264729c9221cdf43869fd820430bf938bc
vagabondHustler <vagabondHustler.github@gmail.com>
2022-09-09 19:21:35 +0200

    Tests update test to pass

--- tests/test_data_set_data_.py
+++ tests/test_data_set_data_.py
@@ -1,13 +1,13 @@
 import os
 
 from src.subsearch.data import (
-    __buttons__,
     __data__,
     __gui__,
     __home__,
-    __icons__,
+    __icon__,
     __providers__,
     __tabs__,
+    __titlebar__,
     __utils__,
 )
 
@@ -16,8 +16,8 @@ def test_paths() -> None:
     assert __home__ == os.path.join(os.getcwd(), "src\\subsearch")
     assert __data__ == os.path.join(os.getcwd(), "src\\subsearch\\data")
     assert __gui__ == os.path.join(os.getcwd(), "src\\subsearch\\gui")
-    assert __providers__ == os.path.join(os.getcwd(), "src\\subsearch\\scraper")
+    assert __providers__ == os.path.join(os.getcwd(), "src\\subsearch\\providers")
     assert __utils__ == os.path.join(os.getcwd(), "src\\subsearch\\utils")
-    assert __icons__ == os.path.join(os.getcwd(), "src\\subsearch\\assets\\icons")
-    assert __buttons__ == os.path.join(os.getcwd(), "src\\subsearch\\assets\\buttons")
-    assert __tabs__ == os.path.join(os.getcwd(), "src\\subsearch\\assets\\tabs")
+    assert __icon__ == os.path.join(os.getcwd(), "src\\subsearch\\gui\\assets\\icon\\subsearch.ico")
+    assert __titlebar__ == os.path.join(os.getcwd(), "src\\subsearch\\gui\\assets\\titlebar")
+    assert __tabs__ == os.path.join(os.getcwd(), "src\\subsearch\\gui\\assets\\tabs")
