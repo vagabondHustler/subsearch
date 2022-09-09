403a2458ff4868fe67c3b406740f193e81700431
vagabondHustler <vagabondHustler.github@gmail.com>
2022-09-09 19:06:58 +0200

    Feature add titlebar asset path

--- src/subsearch/data/__init__.py
+++ src/subsearch/data/__init__.py
@@ -1,11 +1,11 @@
 from .__set_paths__ import (
-    __buttons__,
     __data__,
     __gui__,
     __home__,
-    __icons__,
+    __icon__,
     __providers__,
     __tabs__,
+    __titlebar__,
     __utils__,
 )
 from .__set_video__ import get_video_file_data
