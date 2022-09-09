87efc364b146547cf195d702d7487fad1a845763
vagabondHustler <vagabondHustler.github@gmail.com>
2022-09-09 19:08:04 +0200

    Feature add load .tcl files

---
+++ src/subsearch/gui/__init__.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+
+inited = False
+root = None
+
+
+def init(func):
+    def wrapper(*args, **kwargs):
+        global inited
+        global root
+
+        if not inited:
+            from tkinter import _default_root
+
+            path = (Path(__file__).parent / "assets" / "ttk_style.tcl").resolve()
+
+            try:
+                _default_root.tk.call("source", str(path))
+            except AttributeError:
+                raise RuntimeError(
+                    "can't set theme. Tk is not initialized. "
+                    + "Please first create a tkinter.Tk instance, then set the theme."
+                ) from None
+            else:
+                inited = True
+                root = _default_root
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@init
+def set_theme(theme):
+    if theme not in {"dark"}:
+        raise RuntimeError(f"not a valid theme name: {theme}")
+
+    root.tk.call("set_theme", theme)
+
+
+@init
+def get_theme():
+    theme = root.tk.call("ttk::style", "theme", "use")
+
+    try:
+        return {"ttk_style": "dark"}[theme]
+    except KeyError:
+        return theme
+
+
+@init
+def toggle_theme():
+    if get_theme() == "dark":
+        use_dark_theme()
+    else:
+        use_dark_theme()
+
+
+use_dark_theme = lambda: set_theme("dark")
