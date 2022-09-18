120ec385370007c126f9d782aa4f4e1eab4d13b1
vagabondHustler <vagabondhustler.github@gmail.com>
2022-09-18 17:37:08 +0200

    Fix scraping with hash never finds a match

--- src/subsearch/providers/opensubtitles.py
+++ src/subsearch/providers/opensubtitles.py
@@ -16,9 +16,7 @@ class OpenSubtitles(BaseProvider):
         self.logged_and_sorted: list[FormattedData] = []
 
     def parse_hash_results(self):
-        to_be_downloaded: list[str] | None = self.scrape.with_hash(
-            self.url_opensubtitles_hash, self.current_language, self.hi_sub
-        )
+        to_be_downloaded = self.scrape.with_hash(self.url_opensubtitles_hash, self.release)
         if to_be_downloaded is None and self.series:
             log.output(f"No TV-series found matching hash {self.file_hash}")
             log.output(f"Done with tasks\n")
@@ -27,9 +25,7 @@ class OpenSubtitles(BaseProvider):
             log.output(f"No movies found matching hash {self.file_hash}")
             log.output(f"Done with tasks\n")
             return None
-
-        download_info = []
-        log.output(f"Preparing  hash {self.file_hash} for download")
+        log.output(f"[100%  match]: {self.release}")
         download_info = generic.named_tuple_zip_data("opensubtitles", __video__.tmp_directory, to_be_downloaded)
         log.output(f"Done with tasks\n")
         return download_info
@@ -79,37 +75,13 @@ class OpenSubtitlesScrape(OpenSubtitles):
             subtitles[release_name] = dl_url
         return subtitles
 
-    def with_hash(self, url: str, current_language: str, hearing_impaired: Union[str, bool]) -> list[str] | None:
-        download_url: list[str] = []
+    def with_hash(self, url: str, release: str) -> dict[str, str] | None:
+        subtitles: dict[str, str] = {}
         doc = generic.get_lxml_doc(url, "lxml")
-        doc_results = doc.find("table", id="search_results")
+        doc_results = doc.find("a", download="download", id="bt-dwl-bt")
         if doc_results is None:
             return None
-        tr_name = doc_results.find_all("tr", id=lambda value: value and value.startswith("name"))
-        for item in tr_name:
-            tl = [a["title"] for a in item.find_all("a", title=current_language)]
-            if current_language not in tl:
-                continue
-            hi_site = item.find("img", alt="Subtitles for hearing impaired")
-            if hi_site is not None and hearing_impaired is False:
-                log.output(f"Found HI-subtitle but skipping, 'cus hearing impaired is set to '{hearing_impaired}'")
-                continue
-            if hi_site is None and hearing_impaired is True:
-                log.output(f"Found nonHI-subtitle but skipping, 'cus hearing impaired is set to '{hearing_impaired}'")
-                continue
-
-            title_name = item.find("a", class_="bnone").text.replace("\n", "").replace("\t", "").replace("(", " (")
-            log.output(f"{title_name} matched file hash")
-            th = [
-                a["href"]
-                for a in item.find_all(
-                    "a",
-                    href=lambda value: value and value.startswith("/en/subtitleserve/sub/"),
-                )
-            ]
-            th = th[0] if th is not None else None
-            link = f"https://www.opensubtitles.org{th}"
-            download_url.append(link) if th is not None else None
-        if len(download_url) == 0:
-            return None
-        return download_url
+        sub_id = doc_results.attrs['data-product-id']
+        download_url = f"https://dl.opensubtitles.org/en/download/sub/{sub_id}"
+        subtitles[release] = download_url
+        return subtitles
