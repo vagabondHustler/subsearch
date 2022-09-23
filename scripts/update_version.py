28d91a8e422c08f0b029fcd6b01cd4c499a76715
vagabondHustler <vagabondhustler.github@gmail.com>
2022-09-24 00:09:09 +0200

    Refactor add pathlib instead of os

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -1,15 +1,15 @@
-import os
 import re
 import sys
+from pathlib import Path
 
-cwd = os.getcwd()  # get current working directory for workflow
+
 version = sys.argv[-1].replace("v", "")  # new version number
-file_path = f"{cwd}/src/subsearch/data/__version__.py"
+file_path = Path(Path.cwd()) / "src" / "subsearch" / "data" / "version.py"
 
 
 with open(file_path, "r+") as f:
     file_content = f.read()  # open file and read the content
-    expression = '(\d*\.\d*\.\d*-\w*\d*)|(\d*\.\d*\.\d*-\w*\.\d*)|(\d*\.\d*\.\d*)'
+    expression = "(\d*\.\d*\.\d*-\w*\d*)|(\d*\.\d*\.\d*-\w*\.\d*)|(\d*\.\d*\.\d*)"
     pattern = re.compile(expression)  # semantic expression https://regex101.com/r/M4qItH/2
     new_content = pattern.sub(version, file_content)  # replace current version number with new version number
     f.seek(0)
