1be91c672ae23117a14aa085a8a96f4506036bab
vagabondHustler <vagabondhustler.github@gmail.com>
2022-09-25 20:28:45 +0200

    Feature log title_data_result & subtitle_result

--- src/subsearch/providers/yifysubtitles.py
+++ src/subsearch/providers/yifysubtitles.py
@@ -1,9 +1,9 @@
 from subsearch.data import __video__
 from subsearch.data.data_fields import (
-    FileSearchData,
     FormattedData,
     ProviderUrlData,
-    UserConfigData,
+    ReleaseData,
+    UserData,
 )
 from subsearch.providers import generic
 from subsearch.providers.generic import BaseProvider
@@ -11,18 +11,25 @@ from subsearch.utils import log, string_parser
 
 
 class YifiSubtitles(BaseProvider):
-    def __init__(self, parameters: FileSearchData, user_parameters: UserConfigData, provider_url: ProviderUrlData):
+    def __init__(self, parameters: ReleaseData, user_parameters: UserData, provider_url: ProviderUrlData):
         BaseProvider.__init__(self, parameters, user_parameters, provider_url)
         self.scrape = YifySubtitlesScrape()
         self.logged_and_sorted: list[FormattedData] = []
 
     def parse_site_results(self):
+        # search for title
         subtitle_data = self.scrape.get_subtitle(self.url_yifysubtitles, self.current_language)
         to_be_downloaded: dict[str, str] = {}
         to_be_sorted: list[FormattedData] = []
+        
+        data_found = True if subtitle_data else False
+        log.output_title_data_result(data_found)
+        if data_found is False:
+            return []
+        # search for subtitle
         for key, value in subtitle_data.items():
             pct_result = string_parser.get_pct_value(key, self.release)
-            self.log.match(pct_result, key)
+            log.output_match(pct_result, key)
             formatted_data = generic.format_key_value_pct("yifysubtitles", key, value, pct_result)
             to_be_sorted.append(formatted_data)
             if self.is_threshold_met(key, pct_result) is False:
@@ -30,14 +37,14 @@ class YifiSubtitles(BaseProvider):
             if value in to_be_downloaded.values():
                 continue
             to_be_downloaded[key] = value
+            
         self.logged_and_sorted = generic.log_and_sort_list("yifysubtitles", to_be_sorted, self.pct_threashold)
-
+        log.output_subtitle_result(to_be_downloaded, to_be_sorted)
         if not to_be_downloaded:
-            self.log.no_subtitles_found(self.release)
-            return None
+            return []
 
+        # pack download data
         download_info = generic.pack_download_data("yifysubtitles", __video__.tmp_directory, to_be_downloaded)
-        self.log.done_with_tasks(end_new_line=True)
         return download_info
 
     def _sorted_list(self):
@@ -59,9 +66,9 @@ class YifySubtitlesScrape(YifiSubtitles):
             subtitle_language = class_a1.contents[3].text
             if current_language.lower() != subtitle_language.lower():
                 continue
-            _releases = class_a1.contents[5].contents[1].contents[3::2]
-            _releases_stripped = list(map(lambda release: release.strip(), _releases))
-            _releases_no_space = list(map(lambda release: release.replace(" ", "."), _releases_stripped))
+            _releases_string = class_a1.contents[5].contents[1].text.replace("subtitle ", "")
+            _releases_list = _releases_string.split("\n")
+            _releases_no_space = list(map(lambda release: release.replace(" ", "."), _releases_list))
             _release_lower = list(map(lambda release: release.lower(), _releases_no_space))
             for release in _release_lower:
                 subtitle_url = class_a1.contents[5].contents[1].attrs["href"].replace("subtitles", "subtitle")
