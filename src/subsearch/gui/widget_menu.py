196b14242cc6c6d550844f9d393455b6d7ed7ddc
vagabondHustler <vagabondhustler.github@gmail.com>
2022-11-19 16:15:54 +0100

    Feature add class `LogToFile` to tab `TabSettings`

--- src/subsearch/gui/widget_menu.py
+++ src/subsearch/gui/widget_menu.py
@@ -179,6 +179,7 @@ class TabSettings(tk.Frame):
         tab_subsearch.ShowContextMenu(self).pack(anchor="center")
         tab_subsearch.ShowContextMenuIcon(self).pack(anchor="center")
         tab_subsearch.ShowDownloadWindow(self).pack(anchor="center")
+        tab_subsearch.LogToFile(self).pack(anchor="center")
         if current_user.running_from_exe() is False:
             tab_subsearch.ShowTerminalOnSearch(self).pack(anchor="center")
         tk.Frame(self, height=20, bg=TkColor().dark_grey).pack(anchor="center", expand=True)
