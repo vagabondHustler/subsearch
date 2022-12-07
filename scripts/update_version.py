de6bf972051d715956a245f51f021082e32d91d3
vagabondHustler <vagabondhustler.github@gmail.com>
2022-12-07 12:33:32 +0100

    Chore del `.replace("v", "") from `version`
    The letter v is no longer included in the version from pushed tags

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -3,14 +3,14 @@ import sys
 from pathlib import Path
 
 
-version = sys.argv[-1].replace("v", "")  # new version number
+version = sys.argv[-1]
 file_path = Path(Path.cwd()) / "src" / "subsearch" / "data" / "version.py"
 
 
 with open(file_path, "r+") as f:
     file_content = f.read()  # open file and read the content
-    expression = "(\d*\.\d*\.\d*-\w*\d*)|(\d*\.\d*\.\d*-\w*\.\d*)|(\d*\.\d*\.\d*)"
-    pattern = re.compile(expression)  # semantic expression https://regex101.com/r/M4qItH/2
+    expression = "(\d*\.\d*\.\d*\w*\d*)|(\d*\.\d*\.\d*)"
+    pattern = re.compile(expression)  # semantic expression https://regex101.com/r/M4qItH/3
     new_content = pattern.sub(version, file_content)  # replace current version number with new version number
     f.seek(0)
     f.truncate()
