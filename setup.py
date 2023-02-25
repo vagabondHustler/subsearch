a6f95807e56cc59f6905c052f1d34763fa4f5aa9
vagabondHustler <vagabondhustler.github@gmail.com>
2023-02-25 17:15:05 +0100

    Feature add import cx_Freeze after deps are installed

--- setup.py
+++ setup.py
@@ -1,24 +1,37 @@
-import sys
+from importlib import util
 
-from cx_Freeze import Executable, setup
+cx_freeze_installed = util.find_spec("cx_Freeze")
 
-base = "Win32GUI" if sys.platform == "win32" else None
+def _cx_freeze():
+    from cx_Freeze import Executable, setup
 
-setup(
-    options={
-        "bdist_msi": {
-            "upgrade_code": "{F12EBFBB-927E-4D29-818F-F26DB025D16C}",
-            "install_icon": "src/subsearch/gui/assets/icon/subsearch.ico",
-        },
-    },
-    executables=[
+    icon = "src/subsearch/gui/assets/icon/subsearch.ico"
+    bdist_msi_options = {"upgrade_code": "{F12EBFBB-927E-4D29-818F-F26DB025D16C}", "install_icon": icon}
+    executable = [
         Executable(
             "src/subsearch/__main__.py",
-            base=base,
-            target_name=f"Subsearch",
-            icon="src/subsearch/gui/assets/icon/subsearch.ico",
+            base="Win32GUI",
+            target_name="Subsearch",
+            icon=icon,
             shortcut_name="Subsearch",
             shortcut_dir="ProgramMenuFolder",
-        )
-    ],
-)
+        ),
+    ]
+    setup(options={"bdist_msi": bdist_msi_options}, executables=executable)
+
+
+def _setup_tools():
+    import setuptools
+
+    setuptools.setup()
+
+
+
+def setup():
+    if cx_freeze_installed:
+        return _cx_freeze()  
+    return _setup_tools()
+
+
+if __name__ == "__main__":
+    setup()
