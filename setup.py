554d22495e5d4dba47b5fa2c3a1afd5ccbc5744b
vagabondHustler <vagabondhustler.github@gmail.com>
2023-02-25 12:25:05 +0100

    Feature add cx_freeze to setup()

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
-from setuptools import setup
+import sys
 
-setup()
+from cx_Freeze import Executable, setup
+
+base = "Win32GUI" if sys.platform == "win32" else None
+
+setup(
+    options={
+        "bdist_msi": {
+            "upgrade_code": "{F12EBFBB-927E-4D29-818F-F26DB025D16C}",
+            "install_icon": "src/subsearch/gui/assets/icon/subsearch.ico",
+        },
+    },
+    executables=[
+        Executable(
+            "src/subsearch/__main__.py",
+            base=base,
+            target_name=f"Subsearch",
+            icon="src/subsearch/gui/assets/icon/subsearch.ico",
+            shortcut_name="Subsearch",
+            shortcut_dir="ProgramMenuFolder",
+        )
+    ],
+)
