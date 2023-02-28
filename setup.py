0a369613a6e480d24a9d0779d78ebc2dc63096fc
vagabondHustler <vagabondhustler.github@gmail.com>
2023-02-28 15:33:15 +0100

    Feature add registry entries to the MSI Installer
    Now the end user don't have to manually clean up the registry after a uninstall of the software.

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 from importlib import util
 
-cx_freeze_installed = util.find_spec("cx_Freeze")
 
 def _cx_freeze():
+    """
+    Builds an executable and MSI installer using cx_Freeze.
+
+    """
     from cx_Freeze import Executable, setup
 
     icon = "src/subsearch/gui/assets/icon/subsearch.ico"
-    bdist_msi_options = {"upgrade_code": "{F12EBFBB-927E-4D29-818F-F26DB025D16C}", "install_icon": icon}
+    # Define registry keys and entries for context menu integration in Windows Explorer
+    #
+    # The context menu will be added to all file types. This can be later customized
+    # inside the application itself, as the applicability of the context menu is associated
+    # with file extensions and controlled by the `appliesto` entry in the registry.
+    #
+    # For more information on the MSI registry table, see:
+    # https://learn.microsoft.com/en-us/windows/win32/msi/registry-table
+    app_name = "Subsearch"
+    registry_path = rf"Software\Classes\*\shell\Subsearch"
+    script_component = "_cx_executable0__Executable_script_src_subsearch___main__.py_"
+    subsearch_key = (f"{app_name}_key", -1, registry_path, None, None, script_component)
+    subsearch_icon = (f"{app_name}_regz_icon", -1, registry_path, "Icon", None, script_component)
+    subsearch_appliesto = (f"{app_name}_regz_appliesto", -1, registry_path, "AppliesTo", None, script_component)
+    subsearch_command = (f"{app_name}_key_command", -1, rf"{registry_path}\command", None, "", script_component)
+
+    msi_upgrade_code = "{F12EBFBB-927E-4D29-818F-F26DB025D16C}"
+    msi_data = {"Registry": {subsearch_key, subsearch_icon, subsearch_appliesto, subsearch_command}}
+
+    bdist_msi_options = {"upgrade_code": f"{msi_upgrade_code}", "install_icon": icon, "data": msi_data}
+
     executable = [
         Executable(
             "src/subsearch/__main__.py",
@@ -21,15 +44,23 @@ def _cx_freeze():
 
 
 def _setup_tools():
+    """
+    Builds the package using setuptools.
+
+    """
     import setuptools
 
     setuptools.setup()
 
 
-
 def setup():
+    """
+    Determines whether cx_Freeze is installed and calls the appropriate build function.
+
+    """
+    cx_freeze_installed = util.find_spec("cx_Freeze")
     if cx_freeze_installed:
-        return _cx_freeze()  
+        return _cx_freeze()
     return _setup_tools()
 
 
