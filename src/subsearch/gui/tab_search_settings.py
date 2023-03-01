5cb3a57601d856f19d6b8709898ccdcc0b5ad26d
vagabondHustler <vagabondhustler.github@gmail.com>
2023-03-01 22:42:22 +0100

    Refactor RenameBestMatch buttons
    Refactors the RenameBestMatch  class by replacing two buttons with a single toggle button. The toggle button uses the text variable to display the current state of the context menu setting and changes its style based on the state. The enter_button method is updated to bind the button to the appropriate function depending on its current state. The button_set_true and button_set_false methods are also updated to change the style of the button when the state is changed.

--- src/subsearch/gui/tab_search_settings.py
+++ src/subsearch/gui/tab_search_settings.py
@@ -224,45 +224,37 @@ class RenameBestMatch(tk.Frame):
         label = tk.Label(self, text="Rename best match")
         label.configure(bg=GUI_DATA.colors.dark_grey, fg=GUI_DATA.colors.white_grey, font=GUI_DATA.fonts.cas8b)
         label.grid(row=0, column=0, sticky="w", padx=2, pady=2)
-        self.clabel = tk.Label(self, textvariable=self.string_var)
-        self.clabel.configure(bg=GUI_DATA.colors.dark_grey, font=GUI_DATA.fonts.cas8b)
-        self.clabel.grid(row=0, column=1, sticky="nsew", padx=2, pady=2)
-        tk_tools.VarColorPicker(self.string_var, self.clabel)
-        btn_true = ttk.Button(
+        btn_toggle = ttk.Button(
             self,
-            text="True",
-            width=18,
+            textvariable=self.string_var,
+            width=40,
+            style=f"{self.string_var.get()}.TButton",
         )
-        btn_true.grid(row=0, column=3, pady=2)
-        btn_false = ttk.Button(
-            self,
-            text="False",
-            width=18,
-        )
-        btn_false.grid(row=0, column=2, pady=2)
-
-        btn_true.bind("<Enter>", self.enter_button)
-        btn_true.bind("<Leave>", self.leave_button)
-        btn_false.bind("<Enter>", self.enter_button)
-        btn_false.bind("<Leave>", self.leave_button)
+        btn_toggle.grid(row=0, column=2, pady=4, padx=8)
+        btn_toggle.bind("<Enter>", self.enter_button)
+        btn_toggle.bind("<Leave>", self.leave_button)
         tk_tools.set_default_grid_size(self)
 
     def enter_button(self, event):
         btn = event.widget
         if btn["text"] == "True":
-            btn.bind("<ButtonRelease>", self.button_set_true)
+            btn.bind("<ButtonRelease-1>", self.button_set_false)
         if btn["text"] == "False":
-            btn.bind("<ButtonRelease>", self.button_set_false)
+            btn.bind("<ButtonRelease-1>", self.button_set_true)
 
     def leave_button(self, event):
         btn = event.widget
 
     def button_set_true(self, event):
-        self.string_var.set(f"True")
-        tk_tools.VarColorPicker(self.string_var, self.clabel)
+        btn = event.widget
+        self.string_var.set(f"False")
+        btn["style"] = f"{self.string_var.get()}.TButton"
         raw_config.set_config_key_value("rename_best_match", True)
+        self.enter_button(event)
 
     def button_set_false(self, event):
+        btn = event.widget
         self.string_var.set(f"False")
-        tk_tools.VarColorPicker(self.string_var, self.clabel)
+        btn["style"] = f"{self.string_var.get()}.TButton"
         raw_config.set_config_key_value("rename_best_match", False)
+        self.enter_button(event)
