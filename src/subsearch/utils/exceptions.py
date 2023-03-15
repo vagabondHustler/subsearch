2a41be0767535b94bae1b2fd6e6661b088cd3f66
vagabondHustler <vagabondhustler.github@gmail.com>
2023-03-15 22:45:38 +0100

    Improve subsearch error handling and expand provider support
    - Updated documentation for exceptions in `utils/exceptions.py`
    - Moved unsupported providers into a new subclass `ProviderNotImplemented`
    - Improved error message for `CaptchaError` to increase clarity

--- src/subsearch/utils/exceptions.py
+++ src/subsearch/utils/exceptions.py
@@ -1,14 +1,18 @@
 class Error(Exception):
     """Base class for exceptions in subsearch"""
-
     pass
 
 
 class ProviderError(Error):
-    """Base class for errors by providers"""
-
+    """Base class for errors raised by subtitle providers."""
     pass
 
-
 class CaptchaError(ProviderError):
-    """Exception raised by provider when captcha is detected"""
+    """Error raised when a captcha challenge is encountered."""
+    def __init__(self, message="Captcha challenge encountered"):
+        super().__init__(message)
+
+class ProviderNotImplemented(ProviderError):
+    """Error raised when using an unsupported subtitle provider."""
+    def __init__(self, message="This provider is not implemented"):
+        super().__init__(message)
