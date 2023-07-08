970ef623950426c106e9265d430598593c6c2f6f
vagabondHustler <vagabondhustler.github@gmail.com>
2023-07-08 03:41:00 +0200

    Feat add state machine for subsearch core
    - Implemented a state machine to manage the different states of the subsearch core.
    - Updated the `Initializer` class to inherit from the `state_machine.Subsearch` class and set the initial state to `INITIALIZING`.
    - Added state transitions and notifications for each state change.
    - Updated the `AppSteps` class to include state changes at different stages of the subsearch process.
    - Modified the skip steps to check for file existence and other conditions based on the state.

--- src/subsearch/core.py
+++ src/subsearch/core.py
@@ -5,19 +5,27 @@ from subsearch.data import __version__, app_paths, video_data
 from subsearch.data.data_objects import DownloadData, PrettifiedDownloadData
 from subsearch.gui import tab_manager
 from subsearch.providers import opensubtitles, subscene, yifysubtitles
-from subsearch.utils import app_integrity, file_manager, io_json, log, string_parser
+from subsearch.utils import (
+    app_integrity,
+    file_manager,
+    io_json,
+    log,
+    state_machine,
+    string_parser,
+)
 
 
-class Initializer:
+class Initializer(state_machine.Subsearch):
     def __init__(self) -> None:
+        state_machine.Subsearch.__create__(self.UNKNOWN)
+        self.set_state(self.INITIALIZING)
         app_integrity.initialize_application()
         self.app_config = io_json.get_app_config()
         if video_data is not None:
             file_manager.create_directory(video_data.subs_directory)
-            self.file_exist = True
             self.file_hash = file_manager.get_hash(video_data.file_path)
         else:
-            self.file_exist = False
+            self.set_state(self.NO_FILE_FOUND)
             self.file_hash = ""
         self.results: dict[str, list[DownloadData]] = {}
         self.skipped_downloads: dict[str, list[PrettifiedDownloadData]] = {}
@@ -31,6 +39,7 @@ class Initializer:
             self.downloads[provider] = 0
 
         self.ran_download_tab = False
+        self.foreign_only = io_json.get_json_key("foreign_only")
         self.skip_step = SkipStep(self)
         if self.file_exist:
             self.release_data = string_parser.get_release_metadata(video_data.filename, self.file_hash)
@@ -46,6 +55,7 @@ class Initializer:
             )
             log.set_logger_data(**self.search_kwargs)
             log.output_parameters()
+            self.set_state(self.INITIALIZED)
 
     def all_providers_disabled(self) -> bool:
         self.app_config = io_json.get_app_config()
@@ -69,18 +79,19 @@ class AppSteps(Initializer):
         self.start = time.perf_counter()
         Initializer.__init__(self)
         ctypes.windll.kernel32.SetConsoleTitleW(f"subsearch - {__version__}")
-        if self.file_exist is False:
+        if not self.file_exist:
             tab_manager.open_tab("search")
             return None
-        self.foreign_only = io_json.get_json_key("foreign_only")
 
         if " " in video_data.filename:
             log.warning_spaces_in_filename()
         log.output_header("Search started")
+        self.set_state(self.INITIALIZING_SEARCH)
 
     def _provider_opensubtitles(self) -> None:
         if self.skip_step.opensubtitles():
             return None
+        self.set_state(self.OPENSUBS_STARTED)
         # log.output_header("Searching on opensubtitles")
         _opensubs = opensubtitles.OpenSubtitles(**self.search_kwargs)
         if self.app_config.providers["opensubtitles_hash"] and self.file_hash != "":
@@ -88,25 +99,31 @@ class AppSteps(Initializer):
         if self.app_config.providers["opensubtitles_site"]:
             self.results["opensubtitles_site"] = _opensubs.parse_site_results()
         self.skipped_downloads["opensubtitles_site"] = _opensubs._sorted_list()
+        self.set_state(self.OPENSUBS_ENDED)
 
     def _provider_subscene(self) -> None:
         if self.skip_step.subscene():
             return None
+        self.set_state(self.SUBSCENE_STARTED)
         _subscene = subscene.Subscene(**self.search_kwargs)
         self.results["subscene_site"] = _subscene.parse_site_results()
         self.skipped_downloads["subscene_site"] = _subscene._sorted_list()
+        self.set_state(self.SUBSCENE_ENDED)
 
     def _provider_yifysubtitles(self) -> None:
         if self.skip_step.yifysubtitles():
             return None
+        self.set_state(self.YIFYSUBS_STARTED)
         _yifysubs = yifysubtitles.YifiSubtitles(**self.search_kwargs)
         self.results["yifysubtitles_site"] = _yifysubs.parse_site_results()
         self.skipped_downloads["yifysubtitles_site"] = _yifysubs._sorted_list()
+        self.set_state(self.YIFYSUBS_ENDED)
 
     def _download_files(self) -> None:
         if self.skip_step.download_files():
             return None
         log.output_header(f"Downloading subtitles")
+        self.set_state(self.DOWNLOADING_FILES)
         for provider, data in self.results.items():
             if self.app_config.providers[provider] is False:
                 continue
@@ -132,13 +149,15 @@ class AppSteps(Initializer):
     def _extract_zip_files(self) -> None:
         if self.skip_step.extract_zip():
             return None
+        self.set_state(self.EXTRACTING_FILES)
         log.output_header("Extracting downloads")
         file_manager.extract_files(app_paths.tmpdir, video_data.subs_directory, ".zip")
         log.output_done_with_tasks(end_new_line=True)
 
     def _clean_up(self) -> None:
-        if self.file_exist is False:
+        if not self.file_exist:
             return None
+        self.set_state(self.CLEANING_UP)
         if self.app_config.rename_best_match:
             log.output_header("Renaming best match")
             file_manager.rename_best_match(f"{self.release_data.release}.srt", video_data.directory_path, ".srt")
@@ -153,6 +172,7 @@ class AppSteps(Initializer):
 
     def _pre_exit(self) -> None:
         elapsed = time.perf_counter() - self.start
+        self.set_state(self.EXITING)
         log.output(f"Finished in {elapsed} seconds")
 
         if self.app_config.show_terminal is False:
@@ -171,7 +191,7 @@ class SkipStep:
         self.cls = cls
 
     def opensubtitles(self) -> bool:
-        if self.cls.file_exist is False:
+        if not self.cls.file_exist:
             return True
         if self.cls.foreign_only:
             return True
@@ -185,7 +205,7 @@ class SkipStep:
         return False
 
     def subscene(self) -> bool:
-        if self.cls.file_exist is False:
+        if not self.cls.file_exist:
             return True
         if io_json.check_language_compatibility("subscene") is False:
             return True
@@ -194,7 +214,7 @@ class SkipStep:
         return False
 
     def yifysubtitles(self) -> bool:
-        if self.cls.file_exist is False:
+        if not self.cls.file_exist:
             return True
         if self.cls.foreign_only:
             return True
@@ -209,14 +229,14 @@ class SkipStep:
         return False
 
     def download_files(self) -> bool:
-        if self.cls.file_exist is False:
+        if not self.cls.file_exist:
             return True
         if not any(self.cls.results.values()):
             return True
         return False
 
     def not_downloaded(self) -> bool:
-        if self.cls.file_exist is False:
+        if not self.cls.file_exist:
             return True
         number_of_downloads = sum(v for v in self.cls.downloads.values())
         if self.cls.app_config.manual_download_fail and number_of_downloads > 0:
@@ -227,7 +247,7 @@ class SkipStep:
         return False
 
     def extract_zip(self) -> bool:
-        if self.cls.file_exist is False:
+        if not self.cls.file_exist:
             return True
         if self.cls.ran_download_tab:
             return True
