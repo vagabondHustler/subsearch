7a36d650452a266ce7752f3818c29dc9ec736944
vagabondHustler <vagabondhustler.github@gmail.com>
2023-07-19 09:05:40 +0200

    Refactor move state machine functionality from system tray module
    - Moved the state machine functionality from the `system_tray` module to its own module `state_machine` for better separation of concerns.
    - Created a new module `state_machine.py` and transferred the `CoreStateMachine` class and its associated methods to the new module.
    - Updated the import statements in relevant files to import from the new `state_machine` module.

--- src/subsearch/utils/state_machine.py
+++ src/subsearch/utils/state_machine.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 from enum import Enum
+from typing import Any
+
+from subsearch.utils import log
 
 
 @dataclass(order=True, frozen=True)
-class States(Enum):
+class CoreState(Enum):
     UNKNOWN: int = 0
     INITIALIZE: int = 1
     INITIALIZED: int = 2
@@ -20,7 +23,7 @@ class States(Enum):
     GUI: int = -2
 
 
-class StateMachine:
+class CoreStateMachine:
     """
     A class representing a state machine.
 
@@ -35,10 +38,10 @@ class StateMachine:
         _next_state(): Sets the next state of the state machine.
     """
 
-    state = States.UNKNOWN
+    state = CoreState.UNKNOWN
 
     def __init__(self) -> None:
-        self.locked_states = [States.NO_FILE.name, States.GUI.name, States.EXIT.name]
+        self.locked_states = [CoreState.NO_FILE.name, CoreState.GUI.name, CoreState.EXIT.name]
 
     def _yeild_state(self):
         current_index = self.state.value
@@ -47,11 +50,25 @@ class StateMachine:
         yield self.state
 
     def set_state(self, state: int):
-        setattr(StateMachine, "state", States(state))
+        setattr(CoreStateMachine, "state", CoreState(state))
+        log.stdout(self.state, print_allowed=False)
 
     def get_state(self):
-        return getattr(StateMachine, "state")
+        return getattr(CoreStateMachine, "state")
 
     def _next_state(self):
         if self.state.name not in self.locked_states:
             next(self._yeild_state())
+
+    def update_state(self) -> None:
+        self._next_state()
+
+    def lock_to_state(self, state: str) -> None:
+        """
+        Locks the system tray to a specific state, even if update_progress_state is called
+
+        Args:
+            state (str): "no_file", "gui", "exit"
+        """
+        states = {"no_file": CoreState.NO_FILE.value, "gui": CoreState.GUI.value, "exit": CoreState.EXIT.value}
+        self.set_state(states[state])
