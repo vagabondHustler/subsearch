73ebd104817d7cd3f08a454b6d827986ca4323d3
vagabondHustler <vagabondhustler.github@gmail.com>
2023-07-19 08:55:06 +0200

    Refactor separate state machine from system tray
    - Extracted the state machine functionality from the `SystemTray` class.
    - Created a new class `StateMachine` to handle the state transitions.
    - Updated the `SystemTray` class to remove state-related methods (`update_progress_state`, `lock_to_state`, `prettified_tooltip`) and instead use the `StateMachine` class.
    - Removed the `_state_to_progressbar` and `_create_progress_bar` methods from the `SystemTray` class.
    
    Note: This commit refactors the code to improve the separation of concerns by separating the state machine logic from the system tray functionality.

--- src/subsearch/gui/system_tray.py
+++ src/subsearch/gui/system_tray.py
@@ -1,11 +1,9 @@
 import threading
-from typing import Literal
-
 import pystray
 from PIL import Image
 
 from subsearch.data import __version__, app_paths
-from subsearch.utils.state_machine import StateMachine, States
+
 
 enable_system_tray: bool
 
@@ -21,7 +19,7 @@ def system_tray_conditions(func):
     return wrapper
 
 
-class SystemTray(StateMachine):
+class SystemTray:
     """
     A class representing a system tray instance.
 
@@ -31,49 +29,16 @@ class SystemTray(StateMachine):
 
     Methods:
         __init__(): Initializes the SystemTray class.
-        update_progress_state(): Updates the progress state of the system tray.
-        lock_to_state(state: str): Locks the system tray to a specific state.
-        prettified_tooltip(): Generates a prettified tooltip for the system tray.
         start(): Starts the system tray instance.
         stop(): Stops the system tray instance.
-        _run_pystray(): Runs the system tray in a separate thread.
-        _state_to_progressbar() -> int: Converts the current state to a progress bar value.
-        _create_progress_bar(progress, width=15): Creates a progress bar string based on the given progress value.
     """
 
     @system_tray_conditions
     def __init__(self) -> None:
-        StateMachine.__init__(self)
-        self.state_machine = StateMachine()
-        title = self.prettified_tooltip()
+        title = f"Subsearch {__version__}"
         icon = Image.open(app_paths.gui_assets / "subsearch.ico")
         self.tray = pystray.Icon("Subsearch", icon=icon, title=title)
         self.thread_tray = threading.Thread(daemon=True, target=self._run_pystray)
-
-    @system_tray_conditions
-    def update_progress_state(self) -> None:
-        self._next_state()
-        self.tray.title = self.prettified_tooltip()
-        self.tray.update_menu()
-
-    @system_tray_conditions
-    def lock_to_state(self, state: str) -> None:
-        """
-        Locks the system tray to a specific state, even if update_progress_state is called
-
-        Args:
-            state (str): "no_file", "gui", "exit"
-        """
-        states = {"no_file": States.NO_FILE.value, "gui": States.GUI.value, "exit": States.EXIT.value}
-        self.set_state(states[state])
-        self.tray.title = self.prettified_tooltip()
-        self.tray.update_menu()
-
-    @system_tray_conditions
-    def prettified_tooltip(self) -> str:
-        pct = self._state_to_progressbar()
-        status = str(self.state).split(".")[-1].capitalize().replace("_", " ")
-        return f" {status}\n{pct}"
 
     @system_tray_conditions
     def toast_message(self, title: str, msg: str):
@@ -91,24 +56,3 @@ class SystemTray(StateMachine):
 
     def _run_pystray(self) -> None:
         self.tray.run()
-
-    def _state_to_progressbar(self) -> int:
-        if self.state.name == States.EXIT.name:
-            pct = 100
-        elif self.state.name == States.UNKNOWN.name or self.state.name in self.locked_states:
-            pct = 0
-        else:
-            state_values = [state.value for state in States if state.name not in self.locked_states]
-            total_states = len(state_values)
-            current_index = state_values.index(self.state.value)
-            pct = int((current_index) / total_states * 100)
-
-        return self._create_progress_bar(pct)
-
-    def _create_progress_bar(self, progress, width=15):
-        width_filled = int(progress / 100 * width)
-        width_empty = width - width_filled
-        bar_filled = "█" * width_filled
-        bar_empty = "░" * width_empty
-        bar = f"|{bar_filled}{bar_empty}|"
-        return bar
