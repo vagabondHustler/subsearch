5d53c91881f79977ba5b31422da82a0a64968052
vagabondHustler <vagabondhustler.github@gmail.com>
2023-07-21 16:57:35 +0200

    Initial commit

---
+++ src/subsearch/data/constants.py
@@ -0,0 +1,7 @@
+from subsearch.utils import io_app
+
+DEVICE_INFO = io_app.get_system_info()
+VIDEO_FILE = io_app.get_video_file_data()
+APP_PATHS = io_app.get_app_paths()
+SUPPORTED_FILE_EXT = io_app.get_supported_file_ext()
+SUPPORTED_PROVIDERS = io_app.get_supported_providers()
