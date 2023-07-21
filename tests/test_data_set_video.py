f38b0e5fa83a3980240580ffc5f87eccd91a54e3
vagabondHustler <vagabondhustler.github@gmail.com>
2023-07-21 17:44:12 +0200

    Chore del outdated tests
    - Removed outdated unit tests for file management and string parsing since they are no longer relevant to the current implementation.
    - The test files `test_data_set_data_.py`, `test_data_set_video.py`, `test_utils_file_manager.py`, `test_utils_raw_registry.py`, and `test_utils_string_parser.py` were deleted.
    - The tests were removed to ensure that the test suite only contains relevant and up-to-date tests.

--- tests/test_data_set_video.py
+++
@@ -1,5 +0,0 @@
-from src.subsearch.data import video_data
-
-
-def test_paths() -> None:
-    assert video_data is None
--- tests/test_utils_raw_registry.py
+++
@@ -1,8 +0,0 @@
-from src.subsearch.utils import io_winreg
-
-
-def test_got_key() -> None:
-    """
-    test to ensure that the src/subsearch/utils/io_winreg.get_key function returns boolean
-    """
-    assert io_winreg.registry_key_exists() or io_winreg.registry_key_exists() is False
