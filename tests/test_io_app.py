7c072c68fd682d815eeefc82ce74701770be095b
vagabondHustler <vagabondhustler.github@gmail.com>
2023-08-19 00:54:36 +0200

    Chore del all docstrings
    - Removed all existing docstrings, as the majority were outdated. Some new docstrings will be incorporated in the future.

--- tests/test_io_app.py
+++ tests/test_io_app.py
@@ -5,10 +5,6 @@ from subsearch.utils import io_app
 
 
 def test_app_paths() -> None:
-    """Test function for APP_PATHS module.
-
-    This function tests whether various path constants defined in APP_PATHS module are correctly defined.
-    """
     app_paths = io_app.get_app_paths()
     assert app_paths.home.is_dir()
     assert app_paths.data.is_dir()
--- tests/test_utils_version.py
+++ tests/test_utils_version.py
@@ -4,9 +4,5 @@ from subsearch.data import __version__
 
 
 def test_current() -> None:
-    """
-    test so to ensure that the src/subsearch/utils/version.current function returns a with the correct format
-    """
-
     version_digits = "".join(re.findall(r"\d+", __version__))
     assert version_digits.isnumeric() is True
