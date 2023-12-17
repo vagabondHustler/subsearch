34f47ed9cdc6cad139ad287655aae47550b2273e
vagabondHustler <vagabondhustler.github@gmail.com>
2023-12-17 16:51:17 +0100

    Chore ensure `tools` module is found

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from tools import setup_logic
+import sys
+from pathlib import Path
+
+sys.path.insert(0, Path("tools").resolve().parent.as_posix())
 
 
 def setup():
+    from tools import setup_logic
+
     setup_logic.logic()
 
 
