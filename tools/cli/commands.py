5a872b0bc61430ef796b1435a1fcc394108fbb4c
vagabondHustler <vagabondhustler.github@gmail.com>
2023-12-20 16:47:53 +0100

    Refactor update CLI commands structure
    - Reorganized CLI commands into separate functions
    - Improved command parsing and error handling
    - Added individual error handling functions for each command

--- tools/cli/commands.py
+++ tools/cli/commands.py
@@ -1,179 +1,248 @@
 import argparse
+from argparse import ArgumentParser, Namespace
 import functools
-from pathlib import Path
-
-from subsearch.utils import io_log
 from tools.cli.globals import (
-    EXE_BUILD,
-    EXE_INSTALLED,
-    MSI_ARTIFACT,
-    MSI_DIST,
+    EXE_BUILD_PATH,
+    EXE_INSTALLED_PATH,
+    FILE_PATHS,
+    MSI_ARTIFACT_PATH,
+    MSI_DIST_PATH,
+    TEST_NAMES,
     VERSION_CONTROL_PATH,
+    VERSION_PATTERN,
+    VERSION_PYTON_PATH,
 )
 from tools.cli.handlers import (
+    binaries,
+    changelog,
     github_actions,
-    io_changelog,
     io_json,
     io_python,
-    software_deployment,
+    log,
 )
 
-VERSION_PYTON_PATH = Path(Path.cwd()) / "src" / "subsearch" / "data" / "version.py"
-VERSION_REGEX_PATTERN = r"(\d*\.\d*\.\d*[a-zA-Z]*\d*)|(\d*\.\d*\.\d*)"
 
-
-def main() -> None:
-    """
-    Main entry point for the Subsearch CLI tool.
-    """
-    parser = argparse.ArgumentParser(description="Subsearch CLI Tool")
-    subparsers = parser.add_subparsers(title="subcommands", dest="subcommand")
-    file_paths = ["exe_installed", "exe_build", "msi_dist", "msi_artifact"]
-    test_names = ["install", "executable", "uninstall"]
-    # Software Deployment Subcommand
-    software_parser = subparsers.add_parser("software", description="Software Deployment Helper")
-    software_parser.add_argument(
-        "-r",
-        "--read-hashes",
-        dest="get_hash",
-        action="store_true",
-        help="Calculate and create a workflow step output of the hash of the selected file",
+def _parser_init(subparsers: ArgumentParser) -> ArgumentParser:
+    subparser = subparsers.add_parser("init")
+    subparser.add_argument(
+        "-vi",
+        "--validate-inputs",
+        dest="validate_inputs",
+        help="Validate the workflow_dispatch inputs",
     )
-    software_parser.add_argument(
+    return subparser
+
+
+def _parser_binaries(subparsers: ArgumentParser) -> ArgumentParser:
+    subparser = subparsers.add_parser("binaries")
+    subparser.add_argument(
+        "-r",
+        "--read-hash",
+        dest="read_hash",
+        action="store_true",
+        help="Set Github Action workflow out put hash=value",
+    )
+    subparser.add_argument(
         "-w",
-        "--write-hashes",
-        dest="write_hashes",
+        "--write-hash",
+        dest="write_hash",
         action="store_true",
         help="Creates and writes values to hashes.sha256",
     )
-    software_parser.add_argument(
+    subparser.add_argument(
         "-rt",
         "--run-test",
         dest="run_test",
         action="store_true",
         help="Run a test on the selected file",
     )
-    software_parser.add_argument(
+    subparser.add_argument(
         "-f",
         "--file-path",
         dest="file_path",
-        choices=file_paths,
-        help=f"Specify a file path, one of: {file_paths}",
+        choices=FILE_PATHS,
+        help=f"Specify a file path, one of: {FILE_PATHS}",
         required=False,
     )
-    software_parser.add_argument(
+    subparser.add_argument(
         "-t",
         "--test-name",
         dest="test_name",
-        choices=test_names,
-        help=f"Specify a test name: {test_names}",
+        choices=TEST_NAMES,
+        help=f"Specify a test name: {TEST_NAMES}",
         required=False,
     )
+    subparser.add_argument(
+        "-pba",
+        "--prepare-build-artifacts",
+        dest="prepare_build_artifacts",
+        action="store_true",
+        help=f"Move {MSI_DIST_PATH}, {EXE_BUILD_PATH} to ./artifacts",
+        required=False,
+    )
+    return subparser
 
-    # JSON Subcommand
-    json_parser = subparsers.add_parser("json", description="JSON manipulation CLI")
-    json_parser.add_argument(
+
+def _parser_json(subparsers: ArgumentParser) -> ArgumentParser:
+    subparser = subparsers.add_parser("json")
+    subparser.add_argument(
         "-f",
         "--file",
         type=str,
         default=VERSION_CONTROL_PATH,
         help="Path to the JSON file",
     )
-    json_parser.add_argument(
+    subparser.add_argument(
         "-r",
         "--read",
         type=str,
         help="Read the value associated with the specified key",
     )
-    json_parser.add_argument(
+    subparser.add_argument(
         "-w",
         "--write",
         nargs=2,
         metavar=("key", "value"),
         help="Set the value of the specified key",
     )
-    # Python Subcommand
-    python_parser = subparsers.add_parser("python", description="PY manipulation CLI")
-    python_parser.add_argument(
+    return subparser
+
+
+def _parser_python(subparsers: ArgumentParser) -> ArgumentParser:
+    subparser = subparsers.add_parser("python")
+    subparser.add_argument(
+        "-f",
         "--file",
         dest="file",
         default=VERSION_PYTON_PATH,
         help="Path to the python file.",
     )
-    python_parser.add_argument(
+    subparser.add_argument(
+        "-p",
         "--pattern",
         dest="pattern",
-        default=VERSION_REGEX_PATTERN,
+        default=VERSION_PATTERN,
         help="Regex pattern for extracting information.",
     )
-    python_parser.add_argument(
-        "--new-string",
-        dest="new_string",
-        help="New string to update in the file.",
+    subparser.add_argument(
+        "-ws",
+        "--write-string",
+        dest="write_string",
+        help="Update string in the file.",
     )
-    python_parser.add_argument(
+    subparser.add_argument(
+        "-rs",
         "--read-string",
         dest="read_string",
+        action="store_true",
         help="Sets adds a new output in the workflow string=value",
     )
+    return subparser
 
-    # New Changelog Subcommand
-    changelog_parser = subparsers.add_parser("changelog", description="Changelog Update Helper")
-    changelog_parser.add_argument(
-        "--new-tags",
+
+def _parser_changelog(subparsers: ArgumentParser) -> ArgumentParser:
+    subparser = subparsers.add_parser("changelog")
+    subparser.add_argument(
+        "--tags",
         type=str,
         required=True,
-        help="New version tags for the changelog update",
+        help="New tags and latest stable release tags",
     )
-    changelog_parser.add_argument(
+    subparser.add_argument(
         "--file-names",
         type=str,
         required=True,
         help="File names for the changelog update (semicolon-separated)",
     )
-    changelog_parser.add_argument(
+    subparser.add_argument(
         "--hashes",
         type=str,
         required=True,
         help="Hashes for the changelog update (semicolon-separated)",
     )
-
-    args = parser.parse_args()
-
-    if args.subcommand == "software":
-        subargs = software_parser.parse_args()
-        if subargs.get_hash and subargs.run_test:
-            software_parser.error("Cannot use --get-hash and --run-test together.")
-
-        if subargs.get_hash and not subargs.file_path:
-            software_parser.error(f"--get-hash requires specifying one of {file_paths}.")
-
-        if subargs.test_name:
-            if subargs.test_name == "executable" and subargs.file_path:
-                software_parser.error(f"Cannot specify both --test-name executable and one of {file_paths}.")
-            elif subargs.test_name != "executable" and not subargs.file_path:
-                software_parser.error(f"--test-name requires specifying one of {file_paths}.")
-        software_deployment_main(args)
-    elif args.subcommand == "json":
-        io_json_main(args)
-    elif args.subcommand == "python":
-        io_python_main(args)
-    elif args.subcommand == "changelog":
-        io_changelog_main(args)
+    return subparser
 
 
+def _init_error_handle(args: Namespace, parser: ArgumentParser) -> None:
+    ...
 
-def io_json_main(args: argparse.Namespace) -> None:
+
+def _binaries_error_handle(args: Namespace, parser: ArgumentParser) -> None:
+    if args.read_hash and args.run_test:
+        parser.error("Cannot use --get-hash and --run-test together.")
+    if args.read_hash and not args.file_path:
+        parser.error(f"--get-hash requires specifying one of {FILE_PATHS}.")
+    if args.test_name:
+        if args.test_name == "executable" and args.file_path:
+            parser.error(f"Cannot specify both --test-name executable and one of {FILE_PATHS}.")
+        elif args.test_name != "executable" and not args.file_path:
+            parser.error(f"--test-name requires specifying one of {FILE_PATHS}.")
+
+
+def _json_error_handle(args: Namespace, parser: ArgumentParser) -> None:
+    ...
+
+
+def _python_error_handle(args: Namespace, parser: ArgumentParser) -> None:
+    ...
+
+
+def _changelog_error_handle(args: Namespace, parser: ArgumentParser) -> None:
+    ...
+
+
+def _init_parse(args: Namespace) -> None:
+    if args.validate_inputs:
+        log.verbose_print("Validating inputs of workflow dispatch")
+        passed = github_actions.validate_workflow_dispatch_inputs(args.validate_inputs)
+        if passed:
+            log.verbose_print("Everything seems good")
+        else:
+            raise Exception("Something went wrong")
+
+
+def _binaries_parse(args: Namespace) -> None:
+    selected_path = None
+    if args.prepare_build_artifacts:
+        binaries.prepare_build_artifacts()
+    if args.write_hash:
+        binaries.write_to_hashes()
+    if args.file_path:
+        paths = {
+            "exe_installed": EXE_INSTALLED_PATH,
+            "exe_build": EXE_BUILD_PATH,
+            "msi_dist": MSI_DIST_PATH,
+            "msi_artifact": MSI_ARTIFACT_PATH,
+        }
+
+        selected_path = paths.get(args.file_path)
+
+    if args.read_hash:
+        value = binaries.calculate_sha256(selected_path)
+        log.verbose_print(f"New output availible: hash={value}")
+        github_actions.set_step_output("hash", f"{value}")
+
+    elif args.run_test and args.test_name:
+        tests = {
+            "install": functools.partial(binaries.test_msi_package, "install", selected_path),
+            "executable": functools.partial(binaries.test_executable),
+            "uninstall": functools.partial(binaries.test_msi_package, "uninstall", selected_path),
+        }
+        tests[args.test_name]()
+        binaries.set_test_result(args.test_name)
+
+
+def _json_parse(args: Namespace) -> None:
     """
     Main function for the JSON manipulation CLI.
 
     Args:
-        args (argparse.Namespace): Parsed command-line arguments.
+        args (Namespace): Parsed command-line arguments.
     """
     if args.read:
         value = io_json.load_json_value(args.file, args.read)
         print(f"Key: {args.read}, Value: {value}")
-        github_actions.set_output("tags", f"{value}")
+        github_actions.set_step_output("tags", f"{value}")
 
     elif args.write:
         key, value = args.write
@@ -181,69 +250,53 @@ def io_json_main(args: argparse.Namespace) -> None:
         print(f"Value for key '{key}' set to '{value}'.")
 
 
-def io_python_main(args: argparse.Namespace) -> None:
-    """
-    Main function for the Python manipulation CLI.
-
-    Args:
-        args (argparse.Namespace): Parsed command-line arguments.
-    """
+def _python_parse(args: Namespace) -> None:
     value = io_python.read_string(file=args.file, pattern=args.pattern)
     if args.read_string:
-        io_log.verbose_print(f'Result: "{value}"\nPattern: "{args.pattern}"\nFile: "{args.file}"\n')
-        io_log.verbose_print(f"New output availible: tags={value}")
-        github_actions.set_output("tags", f"{value}")
+        log.verbose_print(f'Result: "{value}"\nPattern: "{args.pattern}"\nFile: "{args.file}"\n')
+        log.verbose_print(f"New output availible: tags={value}")
+        github_actions.set_step_output("version", f"{value}")
 
-    if args.new_string:
-        io_log.verbose_print(f"Changed {value} -> {args.new_string}")
-        io_python.write_string(file=args.file, pattern=args.pattern, new_string=args.new_string)
+    if args.write_string:
+        log.verbose_print(f"Changed {value} -> {args.write_string}")
+        io_python.write_string(file=args.file, pattern=args.pattern, write_string=args.write_string)
         print(f'File: "{args.file}" has been updated')
 
 
-def io_changelog_main(args: argparse.Namespace) -> None:
-    """
-    Main function for the Changelog Update Helper.
-
-    Args:
-        args (argparse.Namespace): Parsed command-line arguments.
-    """
-    io_changelog.update_changelog(args.new_tags, args.file_names, args.hashes)
+def _changelog_parse(args: Namespace) -> None:
+    if args.tags and args.file_names and args.hashes:
+        changelog.append_to_changelog(args.tags, args.file_names, args.hashes)
 
 
-def software_deployment_main(args: argparse.Namespace) -> None:
-    """
-    Main function for the Software Deployment Helper.
+def main() -> None:
+    parser = argparse.ArgumentParser(description="CLI for Github Actions workflow")
+    subparsers = parser.add_subparsers(title="subcommands", dest="subcommand")
 
-    Args:
-        args (argparse.Namespace): Parsed command-line arguments.
-    """
-    selected_path = None
-    if args.write_hashes:
-        software_deployment.write_to_hashes()
-    if args.file_path:
-        paths = {
-            "exe_installed": EXE_INSTALLED,
-            "exe_build": EXE_BUILD,
-            "msi_dist": MSI_DIST,
-            "msi_artifact": MSI_ARTIFACT,
-        }
+    init_subparser = _parser_init(subparsers)
+    binaries_subparser = _parser_binaries(subparsers)
+    jason_subparser = _parser_json(subparsers)
+    python_subparser = _parser_python(subparsers)
+    changelog_subparser = _parser_changelog(subparsers)
 
-        selected_path = paths.get(args.file_path)
+    args = parser.parse_args()
 
-    if args.get_hash:
-        value = software_deployment.calculate_sha256(selected_path)
-        io_log.verbose_print(f"New output availible: hash={value}")
-        github_actions.set_output("tags", f"{value}")
-
-    elif args.run_test and args.test_name:
-        tests = {
-            "install": functools.partial(software_deployment.test_msi_package, "install", selected_path),
-            "executable": functools.partial(software_deployment.test_executable),
-            "uninstall": functools.partial(software_deployment.test_msi_package, "uninstall", selected_path),
-        }
-        tests[args.test_name]()
-        software_deployment.set_test_result()(args.test_name)
+    if args.subcommand == "init":
+        _init_error_handle(args, init_subparser)
+        _init_parse(args)
+    if args.subcommand == "binaries":
+        _binaries_error_handle(args, binaries_subparser)
+        _binaries_parse(args)
+    elif args.subcommand == "json":
+        _json_error_handle(args, jason_subparser)
+        _json_parse(args)
+    elif args.subcommand == "python":
+        _python_error_handle(args, python_subparser)
+        _python_parse(args)
+    elif args.subcommand == "changelog":
+        _changelog_error_handle(args, changelog_subparser)
+        _changelog_parse(args)
 
 
 if __name__ == "__main__":
     main()
+
--- tools/cli/globals.py
+++ tools/cli/globals.py
@@ -1,19 +1,28 @@
+import os
 import sys
 from pathlib import Path
 
-from tools.cli.handlers.io_python import read_string
+NEW_VERSION = os.environ["NEW_VERSION"]
+ID = os.environ["ID"]
+VERSON_CONTROL_NAME = os.environ["VERSION_CONTROL_PATH"]
+VERSION_PY_PATH = os.environ["VERSION_PY_PATH"]
+MSI_NAME = os.environ["MSI_NAME"]
+EXE_NAME = os.environ["EXE_NAME"]
+HASHES_NAME = os.environ["HASHES_NAME"]
+CHANGELOG_NAME = os.environ["CHANGELOG_NAME"]
 
 STYLE_SEPERATOR = "-" * 120
-APP_VERISON = read_string()
-EXE_INSTALLED = Path.home() / "AppData" / "Local" / "Programs" / "Subsearch" / "Subsearch.exe"
-EXE_BUILD = Path.cwd() / "build" / f"exe.win-amd64-{sys.version_info[0]}.{sys.version_info[1]}" / "Subsearch.exe"
-MSI_DIST = Path.cwd() / "dist" / f"Subsearch-{APP_VERISON}-win64.msi"
-MSI_ARTIFACT = Path("artifacts") / f"Subsearch-{APP_VERISON}-win64.msi"
-HASHES_PATH = Path.cwd() / "artifacts" / "hashes.sha256"
+VERSION_PATTERN = r"\d+\.\d+\.\d+(dev\d+)?"
+FILE_PATHS = ["exe_installed", "exe_build", "msi_dist", "msi_artifact"]
+TEST_NAMES = ["install", "executable", "uninstall"]
+EXE_INSTALLED_PATH = Path.home() / "AppData" / "Local" / "Programs" / "Subsearch" / EXE_NAME
+EXE_BUILD_PATH = Path.cwd() / "build" / f"exe.win-amd64-{sys.version_info[0]}.{sys.version_info[1]}" / EXE_NAME
+MSI_DIST_PATH = Path.cwd() / "dist" / f"{MSI_NAME}"
+MSI_ARTIFACT_PATH = Path("artifacts") / f"{MSI_NAME}"
+HASHES_PATH = Path.cwd() / "artifacts" / HASHES_NAME
 ARTIFACTS_PATH = Path.cwd() / "artifacts"
-PRE_RELEASE = ["rc", "a", "b", "alpha", "beta", "dev"]
 APP_LOG_PATH = Path.home() / "AppData" / "Local" / "Subsearch" / "log.log"
 APP_CONFIG_PATH = Path.home() / "AppData" / "Local" / "Subsearch" / "config.toml"
 VERSION_CONTROL_PATH = Path.cwd() / ".github" / "configs" / "version_control.json"
-CHANGELOG_BUILDER = Path.cwd() / ".github" / "configs" / "changelog_builder.json"
-PACKAGEPATH = Path(__file__).resolve().parent.as_posix()
+CHANGELOG_BUILDER_PATH = Path.cwd() / ".github" / "configs" / "changelog_builder.json"
+VERSION_PYTON_PATH = Path(Path.cwd()) / "src" / "subsearch" / "data" / "version.py"
