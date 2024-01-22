f5e89f67f05d66b801e49f9324724abfa8e8029d
vagabondHustler <vagabondhustler.github@gmail.com>
2024-01-22 13:49:13 +0100

    Fix log timestamp
    - Updated date formatting in log messages
    - Removed unused variables in `create_logger` method
    - Introduced a singleton class for stdout handling
    - Improved stdout messages for file system actions
    - Enhanced logging for dataclass instances

--- src/subsearch/utils/io_log.py
+++ src/subsearch/utils/io_log.py
@@ -3,6 +3,8 @@ import logging
 from pathlib import Path
 import threading
 from typing import Optional, TypeVar
+from datetime import date
+
 
 from subsearch.globals import metaclasses
 from subsearch.globals.constants import FILE_PATHS, APP_PATHS
@@ -26,18 +28,18 @@ class Logger(metaclass=metaclasses.Singleton):
         if not APP_PATHS.appdata_subsearch.exists():
             APP_PATHS.appdata_subsearch.mkdir(parents=True, exist_ok=True)
         self.log_file_path = kwargs.get("debug_log_file", FILE_PATHS.log)
-        self.datefmt = kwargs.get("datefmt", "%d-%b-%y %H:%M:%S")
-        self.txtfmt = kwargs.get("txtfmt" "%(asctime)s - %(levelname)s - %(message)s")
         self._debug_logger = self.create_logger()
         self._lock = threading.Lock()
         self._ansi = ANSIEscapeSequences
-        
+        today = date.today()
+        self.log(today, print_allowed=False)
+
     def create_logger(self) -> logging.Logger:
         logger = logging.getLogger(self.logger_name)
         logger.setLevel(logging.DEBUG)
         file_handler = logging.FileHandler(self.log_file_path, mode="w")
         file_handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(self.txtfmt, datefmt=self.datefmt)
+        formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
         return logger
@@ -81,57 +83,62 @@ class Logger(metaclass=metaclasses.Singleton):
         print(f"{style_code}{color_code}{message}{self._ansi.RESET}")
 
 
-def stdout(message: str, **kwargs) -> None:
-    end_new_line = kwargs.get("end_new_line", False)
-    _logger = Logger()
-    _logger.log(message, **kwargs)
-    if end_new_line:
-        _logger.log("", **kwargs)
+class StdoutHandler(metaclass=metaclasses.Singleton):
+    def __init__(self) -> None:
+        self._logger = Logger()
+
+    def __call__(self, message: str, **kwargs) -> None:
+        end_new_line = kwargs.get("end_new_line", False)
+        self._logger.log(message, **kwargs)
+        if end_new_line:
+            self._logger.log("", **kwargs)
+
+    def brackets(self, message: str, **kwargs) -> None:
+        self(f"--- [{message}] ---", hex_color="#fab387", style="bold", **kwargs)
+
+    def subtitle_match(self, provider: str, subtitle_name: str, result: int, threshold: int, **kwargs) -> None:
+        if result >= threshold:
+            self(f"> {provider:<14}{result:>3}% {subtitle_name}", hex_color="#a6e3a1")
+        else:
+            self(f"  {provider:<14}{result:>3}% {subtitle_name}")
+
+    def file_system_action(self, action_type: str, src: Path, dst: Optional[Path] = None, **kwargs) -> None:
+        if src.is_file():
+            type_ = "file"
+        elif src.is_dir():
+            type_ = "directory"
+        else:
+            return None
+
+        __src = src.relative_to(src.parent.parent) if src else None
+        __dst = dst.relative_to(dst.parent.parent) if dst else None
+
+        action_messages = {
+            "remove": rf"Removing {type_}: ...\{__src}",
+            "rename": rf"Renaming {type_}: ...\{__src} -> ...\{__dst}",
+            "move": rf"Moving {type_}: ...\{__src} -> ...\{__dst}",
+            "extract": rf"Extracting archive: ...\{__src} -> ...\{__dst}",
+        }
+
+        message = action_messages.get(action_type)
+
+        if not message:
+            raise ValueError("Invalid action type")
+
+        self(message, **kwargs)
+
+    def dataclass(self, instance: DATACLASS, **kwargs) -> None:
+        if not dataclasses.is_dataclass(instance):
+            raise ValueError("Input is not a dataclass instance.")
+        self.brackets(instance.__class__.__name__, **kwargs)
+        for field in dataclasses.fields(instance):
+            key = field.name
+            value = getattr(instance, key)
+            padding = " " * (30 - len(key))
+            self(f"{key}:{padding}{value}", **kwargs)
+
+    def task_completed(self) -> None:
+        self("Task completed", level="info", hex_color="#89b4fa")
 
 
-def stdout_in_brackets(message: str, **kwargs) -> None:
-    stdout(f"--- [{message}] ---", hex_color="#fab387", style="bold", **kwargs)
-
-
-def stdout_match(provider: str, subtitle_name: str, result: int, threshold: int, **kwargs) -> None:
-    if result >= threshold:
-        stdout(f"> {provider:<14}{result:>3}% {subtitle_name}", hex_color="#a6e3a1")
-    else:
-        stdout(f"  {provider:<14}{result:>3}% {subtitle_name}")
-
-
-def stdout_path_action(action_type: str, src: Path, dst: Optional[Path] = None, **kwargs) -> None:
-    if src.is_file():
-        type_ = "file"
-    elif src.is_dir():
-        type_ = "directory"
-    else:
-        return None
-
-    __src = src.relative_to(src.parent.parent) if src else None
-    __dst = dst.relative_to(dst.parent.parent) if dst else None
-
-    action_messages = {
-        "remove": rf"Removing {type_}: ...\{__src}",
-        "rename": rf"Renaming {type_}: ...\{__src} -> ...\{__dst}",
-        "move": rf"Moving {type_}: ...\{__src} -> ...\{__dst}",
-        "extract": rf"Extracting archive: ...\{__src} -> ...\{__dst}",
-    }
-
-    message = action_messages.get(action_type)
-
-    if not message:
-        raise ValueError("Invalid action type")
-
-    stdout(message, **kwargs)
-
-
-def stdout_dataclass(instance: DATACLASS, **kwargs) -> None:
-    if not dataclasses.is_dataclass(instance):
-        raise ValueError("Input is not a dataclass instance.")
-    stdout_in_brackets(instance.__class__.__name__, **kwargs)
-    for field in dataclasses.fields(instance):
-        key = field.name
-        value = getattr(instance, key)
-        padding = " " * (30 - len(key))
-        stdout(f"{key}:{padding}{value}", **kwargs)
+stdout = StdoutHandler()
