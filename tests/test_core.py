f473771e078b056617bd3f2a88bb8eebf1ef50be
vagabondHustler <vagabondhustler.github@gmail.com>
2025-04-10 15:42:37 +0200

    Tests fix call_conditions vars being used instead of `fake_cls`

--- tests/test_core.py
+++ tests/test_core.py
@@ -17,8 +17,9 @@ class FakeSubsearchCore:
         self.language_data = io_toml.load_toml_data(FILE_PATHS.language_data)
         self.accepted_subtitles = []
         self.rejected_subtitles = []
-        self.downloaded_subtitles = 0
-        self.cc = CallConditions(self)
+        self.downloaded_subtitle_archives = 0
+        self.extracted_subtitle_archives = 0
+        self.call_conditions = CallConditions(self)
 
     @property
     def func_name(self) -> str:
@@ -34,123 +35,142 @@ def fake_cls() -> FakeSubsearchCore:
 
 
 def test_conditions_opensubtitles(fake_cls: FakeSubsearchCore) -> None:
-    fake_cls.cc.app_config.providers["opensubtitles_hash"] = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.app_config.providers["opensubtitles_hash"] = False
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
-    fake_cls.cc.app_config.providers["opensubtitles_hash"] = False
-    fake_cls.cc.app_config.providers["opensubtitles_site"] = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    fake_cls.app_config.providers["opensubtitles_hash"] = False
+    fake_cls.app_config.providers["opensubtitles_site"] = False
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
 
 def test_conditions_yifysubtitles(fake_cls: FakeSubsearchCore) -> None:
-    fake_cls.cc.app_config.only_foreign_parts = True
-    fake_cls.cc.release_data.tvseries = True
+    fake_cls.app_config.only_foreign_parts = True
+    fake_cls.release_data.tvseries = True
     fake_cls.provider_urls.yifysubtitles = ""
-    fake_cls.cc.app_config.providers["yifysubtitles_site"] = True
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    fake_cls.app_config.providers["yifysubtitles_site"] = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
-    fake_cls.cc.app_config.only_foreign_parts = False
-    fake_cls.cc.release_data.tvseries = False
+    fake_cls.app_config.only_foreign_parts = False
+    fake_cls.release_data.tvseries = False
     fake_cls.provider_urls.yifysubtitles = "fake_url"
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
 
 def test_conditions_subsource(fake_cls: FakeSubsearchCore) -> None:
-    fake_cls.cc.app_config.only_foreign_parts = True
-    fake_cls.cc.release_data.tvseries = True
-    fake_cls.cc.app_config.providers["subsource_site"] = True
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    fake_cls.app_config.only_foreign_parts = True
+    fake_cls.release_data.tvseries = True
+    fake_cls.app_config.providers["subsource_site"] = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
-    fake_cls.cc.app_config.only_foreign_parts = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.app_config.only_foreign_parts = False
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
 
 def test_conditions_download_files(fake_cls: FakeSubsearchCore) -> None:
-    fake_cls.cc.accepted_subtitles = []
-    fake_cls.cc.app_config.automatic_downloads = True
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
-    
-    fake_cls.cc.accepted_subtitles = ["subtitle1", "subtitle2"]
-    fake_cls.cc.app_config.always_open = False
-    fake_cls.cc.app_config.open_on_no_matches = False
-    fake_cls.cc.app_config.automatic_downloads = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.accepted_subtitles = []
+    fake_cls.app_config.automatic_downloads = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
-    fake_cls.cc.accepted_subtitles = ["subtitle1", "subtitle2"]
-    fake_cls.cc.app_config.always_open = True
-    fake_cls.cc.app_config.automatic_downloads = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    fake_cls.accepted_subtitles = ["subtitle1", "subtitle2"]
+    fake_cls.app_config.always_open = False
+    fake_cls.app_config.open_on_no_matches = False
+    fake_cls.app_config.automatic_downloads = False
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+
+    fake_cls.accepted_subtitles = ["subtitle1", "subtitle2"]
+    fake_cls.app_config.always_open = True
+    fake_cls.app_config.automatic_downloads = False
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
 
 def test_conditions_download_manager(fake_cls: FakeSubsearchCore) -> None:
-    fake_cls.cc.accepted_subtitles = []
-    fake_cls.cc.rejected_subtitles = ["subtitle1"]
-    fake_cls.cc.app_config.open_on_no_matches = True
-    fake_cls.cc.app_config.always_open = False
-    fake_cls.cc.app_config.automatic_downloads = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.accepted_subtitles = []
+    fake_cls.rejected_subtitles = ["subtitle1"]
+    fake_cls.app_config.open_on_no_matches = True
+    fake_cls.app_config.always_open = False
+    fake_cls.app_config.automatic_downloads = False
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
-    fake_cls.cc.app_config.open_on_no_matches = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    fake_cls.app_config.open_on_no_matches = False
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
-    fake_cls.cc.app_config.open_on_no_matches = False
-    fake_cls.cc.app_config.always_open = True
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.app_config.open_on_no_matches = False
+    fake_cls.app_config.always_open = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
-    fake_cls.cc.accepted_subtitles = ["subtitle1"]
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.accepted_subtitles = ["subtitle1"]
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
-    fake_cls.cc.app_config.automatic_downloads = True
-    fake_cls.cc.rejected_subtitles = []
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.app_config.automatic_downloads = True
+    fake_cls.rejected_subtitles = []
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
 
 def test_conditions_extract_files(fake_cls: FakeSubsearchCore) -> None:
-    fake_cls.cc.accepted_subtitles = []
-    fake_cls.cc.rejected_subtitles = ["subtitle1"]
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    fake_cls.downloaded_subtitle_archives = 0
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
-    fake_cls.cc.accepted_subtitles = ["subtitle1", "subtitle2"]
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.accepted_subtitles = ["subtitle1", "subtitle2"]
+    fake_cls.downloaded_subtitle_archives = 0
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+
+    fake_cls.accepted_subtitles = ["subtitle1", "subtitle2"]
+    fake_cls.downloaded_subtitle_archives = 1
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
 
 def test_conditions_subtitle_rename(fake_cls: FakeSubsearchCore) -> None:
-    fake_cls.cc.downloaded_subtitles = 2
-    fake_cls.cc.app_config.subtitle_post_processing["rename"] = True
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.extracted_subtitle_archives = 2
+    fake_cls.app_config.subtitle_post_processing["rename"] = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
-    fake_cls.cc.app_config.subtitle_post_processing["rename"] = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    fake_cls.app_config.subtitle_post_processing["rename"] = False
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
 
 def test_conditions_subtitle_move_best(fake_cls: FakeSubsearchCore) -> None:
-    fake_cls.cc.downloaded_subtitles = 2
-    fake_cls.cc.app_config.subtitle_post_processing["move_best"] = True
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.extracted_subtitle_archives = 1
+    fake_cls.app_config.subtitle_post_processing["move_best"] = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
-    fake_cls.cc.app_config.subtitle_post_processing["move_all"] = True
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    fake_cls.extracted_subtitle_archives = 1
+    fake_cls.app_config.subtitle_post_processing["move_all"] = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
-    fake_cls.cc.app_config.subtitle_post_processing["move_all"] = False
-    fake_cls.cc.app_config.subtitle_post_processing["move_best"] = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    fake_cls.extracted_subtitle_archives = 1
+    fake_cls.app_config.subtitle_post_processing["move_all"] = False
+    fake_cls.app_config.subtitle_post_processing["move_best"] = False
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+
+    fake_cls.extracted_subtitle_archives = 0
+    fake_cls.app_config.subtitle_post_processing["move_all"] = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+
+    fake_cls.extracted_subtitle_archives = 0
+    fake_cls.app_config.subtitle_post_processing["move_best"] = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+
+    fake_cls.extracted_subtitle_archives = 0
+    fake_cls.app_config.subtitle_post_processing["move_best"] = True
+    fake_cls.app_config.subtitle_post_processing["open_on_no_matches"] = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
 
 def test_conditions_subtitle_move_all(fake_cls: FakeSubsearchCore) -> None:
-    fake_cls.cc.downloaded_subtitles = 2
-    fake_cls.cc.app_config.subtitle_post_processing["move_all"] = True
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.extracted_subtitle_archives = 1
+    fake_cls.app_config.subtitle_post_processing["move_all"] = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
-    fake_cls.cc.app_config.subtitle_post_processing["move_all"] = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    # fake_cls.app_config.subtitle_post_processing["move_all"] = False
+    # assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
 
 def test_conditions_summary_notification(fake_cls: FakeSubsearchCore) -> None:
-    fake_cls.cc.app_config.summary_notification = True
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
+    fake_cls.app_config.summary_notification = True
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is True
 
-    fake_cls.cc.app_config.summary_notification = False
-    assert fake_cls.cc.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
+    fake_cls.app_config.summary_notification = False
+    assert fake_cls.call_conditions.call_func(cls=fake_cls, func_name=fake_cls.func_name) is False
 
 
 if __name__ == "__main__":
